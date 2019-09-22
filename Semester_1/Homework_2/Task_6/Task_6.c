3dc6473889b371719492eb608199115b9d5ba9f0
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-09-22 13:12:17 +0300

    Add solution for Task_6

--- Semester_1/Homework_2/Task_6/Task_6.c
+++ Semester_1/Homework_2/Task_6/Task_6.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+
+const int maxSize = 256;
+
+int charToInt(char input)
+{
+    return input - '0';
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int amountOfNumbers[10] = {0};
+    char number[maxSize] = "";
+    printf("Enter the number: ");
+    scanf("%s", &number);
+
+    for (int i = 0; i < strlen(number); i++)
+    {
+        amountOfNumbers[charToInt(number[i])]++;
+    }
+
+    printf("Smallest number: ");
+    for (int i = 1; i < 10; i++)
+    {
+        if (amountOfNumbers[i] > 0)
+        {
+            printf("%d", i);
+            amountOfNumbers[i]--;
+            break;
+        }
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        while (amountOfNumbers[i] > 0)
+        {
+            printf("%d", i);
+            amountOfNumbers[i]--;
+        }
+    }
+
     return 0;
 }
