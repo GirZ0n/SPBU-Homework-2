eee227d0db2deafa8613173e6bf0b7e430d368bb
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-09-22 21:17:35 +0300

    1) Wrote the main function; 2) Initialized numberOfCows function; 3) Initialized numberOfBulls function.

--- Semester_1/Homework_2/Task_3/Task_3.c
+++ Semester_1/Homework_2/Task_3/Task_3.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello, World!\n");
+int numberOfCows()
+{
     return 0;
+}
+
+int numberOfBulls()
+{
+    return 0;
+}
+
+int main()
+{
+
+    printf("I made up a four-digit number. Try to guess it ;) \n");
+
+    srand(time(NULL));
+    int randomNumber = rand() % 10000;
+
+    int inputNumber = 0;
+    int moves = 1;
+    while (true)
+    {
+        printf("Enter the estimated number:");
+        scanf("%d", &inputNumber);
+
+        if (inputNumber == randomNumber)
+        {
+            printf("You WIN! (In %d moves)", moves);
+            return 0;
+        }
+
+        printf("Number of cows: %d \n", numberOfCows());
+        printf("Number of bulls: %d \n", numberOfBulls());
+        moves++;
+    }
 }
