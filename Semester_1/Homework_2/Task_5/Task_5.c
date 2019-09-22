b50b8cf74b7a63a64b12ee5aa302c269497c43ef
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-09-22 15:52:26 +0300

    1) Wrote the main function; 2) Initialized symbolsCount function; 3) Initialized isEqual function.

--- Semester_1/Homework_2/Task_5/Task_5.c
+++ Semester_1/Homework_2/Task_5/Task_5.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+const int maxSize = 256;
+
+void symbolsCount(char string[], int amountOfSymbols[])
+{
+    return;
+}
+
+bool isEqual(int amountOfSymbolsInString1[], int amountOfSymbolsInString2[])
+{
+    return false;
+}
+
+int main()
+{
+    int amountOfSymbolsInString1[256] = {0};
+    int amountOfSymbolsInString2[256] = {0};
+
+    char inputString1[256] = "";
+    printf("Enter the string #1: ");
+    scanf("%s", &inputString1);
+    char inputString2[256] = "";
+    printf("Enter the string #2: ");
+    scanf("%s", &inputString2);
+
+    symbolsCount(inputString1, amountOfSymbolsInString1);
+    symbolsCount(inputString2, amountOfSymbolsInString2);
+
+    if (isEqual(amountOfSymbolsInString1, amountOfSymbolsInString2))
+    {
+        printf("From the first string you CAN get the second!");
+        return 0;
+    }
+
+    printf("From the first line you CANNOT get the second :(");
 }
