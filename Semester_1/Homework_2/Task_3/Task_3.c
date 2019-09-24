76c5be74fd6f007d886f1838107d9eeb3d20f106
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-09-24 23:06:42 +0300

    1) Wrote the numberOfBulls function 2) Wrote indexOfDigitToInt function

--- Semester_1/Homework_2/Task_3/Task_3.c
+++ Semester_1/Homework_2/Task_3/Task_3.c
@@ -2,15 +2,35 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+int indexOfDigitToInt(int number, int index)
+{
+    int digits[4] = {0};
+    digits[0] = number / 1000;
+    digits[1] = number % 1000 / 100;
+    digits[2] = number % 100 / 10;
+    digits[3] = number % 10;
+
+    return digits[index];
+}
 
 int numberOfCows()
 {
     return 0;
 }
 
-int numberOfBulls()
+int numberOfBulls(int randomNumber, int inputNumber)
 {
-    return 0;
+    int count = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        if (indexOfDigitToInt(randomNumber, i) == indexOfDigitToInt(inputNumber, i))
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
 int main()
@@ -20,8 +40,9 @@ int main()
 
     srand(time(NULL));
     int randomNumber = rand() % 10000;
-
     int inputNumber = 0;
+    //printf("%d", randomNumber);
+
     int moves = 1;
     while (true)
     {
@@ -35,7 +56,7 @@ int main()
         }
 
         printf("Number of cows: %d \n", numberOfCows());
-        printf("Number of bulls: %d \n", numberOfBulls());
+        printf("Number of bulls: %d \n", numberOfBulls(randomNumber, inputNumber));
         moves++;
     }
 }
