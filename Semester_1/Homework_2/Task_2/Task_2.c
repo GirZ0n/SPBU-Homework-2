0ccd0b7a92fc0a286fc381615c75ff5a5d9e2f9b
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-09-26 23:26:19 +0300

    Add solution for Task_2

--- Semester_1/Homework_2/Task_2/Task_2.c
+++ Semester_1/Homework_2/Task_2/Task_2.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+struct Fraction
+{
+    int numerator;
+    int denominator;
+};
+
+
+int getGreatestCommonDivisor(int firstNumber, int secondNumber)
+{
+    int remainder = firstNumber % secondNumber;
+
+    if (remainder == 0) {
+        return secondNumber;
+    }
+
+    return getGreatestCommonDivisor(secondNumber, remainder);
+}
+
+
+struct Fraction getIrreducibleFraction(int numerator, int denominator)
+{
+    int greatestCommonDivisor = getGreatestCommonDivisor(numerator, denominator);
+
+    struct Fraction irreducibleFraction;
+    irreducibleFraction.numerator = numerator / greatestCommonDivisor;
+    irreducibleFraction.denominator = denominator / greatestCommonDivisor;
+
+    return irreducibleFraction;
+}
+
 
 int main() {
-    printf("Hello, World!\n");
+    int inputDenominator = 0;
+    printf("Enter the denominator:");
+    scanf("%d", &inputDenominator);
+
+    printf("All simple irreducible fractions in the interval (0, 1): ");
+    struct Fraction currentIrreducibleFraction;
+    currentIrreducibleFraction.numerator = 0;
+    currentIrreducibleFraction.denominator = 0;
+    for (int i = 0; i < inputDenominator - 1; i++)
+    {
+        currentIrreducibleFraction = getIrreducibleFraction(i + 1, inputDenominator);
+
+        printf("%d/%d ; ", currentIrreducibleFraction.numerator, currentIrreducibleFraction.denominator);
+    }
+
     return 0;
 }
