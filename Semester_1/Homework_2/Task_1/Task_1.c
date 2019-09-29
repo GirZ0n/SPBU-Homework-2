0f690ebdf0053989f44b31237de62aa5f62b263f
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-09-29 23:05:10 +0300

    Wrote the main function

--- Semester_1/Homework_2/Task_1/Task_1.c
+++ Semester_1/Homework_2/Task_1/Task_1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void printCompositions(int boo, int maxTerm, int *composition, int index)
+{
+    return;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int number = 0;
+    printf("Enter the number:");
+    scanf("%d", &number);
+
+    printf("The compositions of %d are:\n", number);
+    int *numberComposition = malloc(number * sizeof(int));
+    printCompositions(number, number, &numberComposition, 0);
     return 0;
 }
