ada98d1209030742cf83964e3f88f55489e9af9c
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-09-29 23:45:48 +0300

    Wrote the printCompositions function

--- Semester_1/Homework_2/Task_1/Task_1.c
+++ Semester_1/Homework_2/Task_1/Task_1.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printCompositions(int boo, int maxTerm, int *composition, int index)
+void printCompositions(int needToAdd, int maxTerm, int *composition, int index)
 {
-    return;
+    if (needToAdd < 0 || maxTerm < 0)
+    {
+        return;
+    }
+
+    if (needToAdd == 0)
+    {
+        for (int i = 0; i < index; i++)
+        {
+            if (i + 1 == index)
+            {
+                printf("%d; \n", composition[i]);
+                return;
+            }
+            else
+            {
+                printf("%d + ", composition[i]);
+            }
+        }
+    }
+
+    for (int i = maxTerm; i > 0; i--)
+    {
+        composition[index] = i;
+        printCompositions(needToAdd - i, i, composition,index + 1);
+    }
 }
 
 int main() {
@@ -13,6 +38,6 @@ int main() {
 
     printf("The compositions of %d are:\n", number);
     int *numberComposition = malloc(number * sizeof(int));
-    printCompositions(number, number, &numberComposition, 0);
+    printCompositions(number, number, numberComposition, 0);
     return 0;
 }
