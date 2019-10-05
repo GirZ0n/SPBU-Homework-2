d82f35b50fb495c77b8b47fbe402898003d6394f
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-10-05 12:21:52 +0300

    Add solution for Task_2

--- Semester_1/Homework_3/Task_2/Task_2.c
+++ Semester_1/Homework_3/Task_2/Task_2.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+const int stringMaxSize = 512;
+
+bool isSpecials(char input)
+{
+    return (input == ' ') || (input == '\n') || (input == '\t');
+}
+
+bool isEmpty(char *string)
+{
+    for (int i = 0; i < strlen(string); i++)
+    {
+        if (!isSpecials(string[i]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *input = fopen("input.txt", "r");
+
+    int numberOfNonEmptyStrings = 0;
+    char *inputString = malloc(sizeof(char) * stringMaxSize);
+    if (input == NULL)
+    {
+        printf("Cant open the file");
+        return 0;
+    }
+    else
+    {
+        while (feof(input) == 0)
+        {
+            fgets(inputString, stringMaxSize, input);
+            if (!isEmpty(inputString))
+            {
+                numberOfNonEmptyStrings++;
+            }
+        }
+    }
+
+    printf("The number of non-empty lines = %d", numberOfNonEmptyStrings);
+
+    free(inputString);
+    fclose(input);
     return 0;
 }
