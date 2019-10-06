c789055ca1126182e78b3c3f5a750997c518f755
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-10-06 18:49:38 +0300

    Some codestyle fixes

--- Semester_1/Homework_3/Task_4/Task_4.c
+++ Semester_1/Homework_3/Task_4/Task_4.c
@@ -4,17 +4,19 @@ int main() {
     int numberOfUsers = 0;
     int numberOfActions = 0;
     int timePeriod = 0;
-    printf("Enter the number of users, the required number of actions and the time interval:");
+    printf("Enter the number of users, the required number of actions and the time interval: ");
     scanf("%d %d %d", &numberOfUsers, &numberOfActions, &timePeriod);
 
     int answer = 0;
     for (int i = 0; i < numberOfUsers; i++)
     {
         int numberOfUserAction = 0;
+        printf("Enter the number of actions the user performed: ");
         scanf("%d", &numberOfUserAction);
 
         int action = 0;
         int count = 0;
+        printf("Enter how many minutes ago each action took place: ");
         for (int j = 0; j < numberOfUserAction; j++)
         {
             scanf("%d", &action);
@@ -30,7 +32,7 @@ int main() {
         }
     }
 
-    printf("Total committed %d action(-s) in the last %d minute(-s): %d user(-s)",
+    printf("Total performed %d action(-s) in the last %d minute(-s): %d user(-s)",
             numberOfActions, timePeriod, answer);
     return 0;
 }
