ab86cbf91d7ce5caebb5e132a426c8f2f69acf53
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-10-08 15:23:40 +0300

    Add solution for Task_1

--- Semester_1/Test_1/Task_1/Task_1.c
+++ Semester_1/Test_1/Task_1/Task_1.c
@@ -1,6 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    printf("Enter the size of Vasya's notes set: ");
+    int sizeOfVasyaNotesSet = 0;
+    scanf("%d", &sizeOfVasyaNotesSet);
+    printf("Enter Vasya's notes set: ");
+    int *vasyaNotes = malloc(sizeof(int) * sizeOfVasyaNotesSet);
+    for (int i = 0; i < sizeOfVasyaNotesSet; i++)
+    {
+        int input = 0;
+        scanf("%d", &input);
+        vasyaNotes[i] = input;
+    }
+
+
+    printf("Enter the size of Petya's notes set: ");
+    int sizeOfPetyaNotesSet = 0;
+    scanf("%d", &sizeOfPetyaNotesSet);
+    printf("Enter Petya's notes set: ");
+    int *petyaNotes = malloc(sizeof(int) * sizeOfPetyaNotesSet);
+    for (int i = 0; i < sizeOfPetyaNotesSet; i++)
+    {
+        int input = 0;
+        scanf("%d", &input);
+        petyaNotes[i] = input;
+    }
+
+    int *result = malloc(sizeof(int) * (sizeOfPetyaNotesSet + sizeOfVasyaNotesSet));
+    int i = 0;
+    int j = 0;
+    while(true)
+    {
+        while (i < sizeOfPetyaNotesSet && petyaNotes[i] >= vasyaNotes[j])
+        {
+            result[i + j] = petyaNotes[i];
+            i++;
+        }
+
+        while (j < sizeOfVasyaNotesSet && vasyaNotes[j] >= petyaNotes[i])
+        {
+            result[i + j] = vasyaNotes[j];
+            j++;
+        }
+
+        if (i == sizeOfPetyaNotesSet && j == sizeOfVasyaNotesSet)
+        {
+            break;
+        }
+    }
+
+    for (int i = 0; i < sizeOfPetyaNotesSet + sizeOfVasyaNotesSet; i++)
+    {
+        printf("%d ", result[i]);
+    }
+
     return 0;
 }
