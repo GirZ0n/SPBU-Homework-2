ac18897ce3eab83f18ad3b1225be678cb103f9e1
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-10-09 22:59:54 +0300

    Add new functons 1) heapSort 2) swap 3) siftDown (only body) 4) buildHeap (only body)

--- Semester_1/Homework_3/Task_1/Task_1.c
+++ Semester_1/Homework_3/Task_1/Task_1.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *elementA, int *elementB)
+{
+    int temp = *elementA;
+    *elementA = *elementB;
+    *elementB = temp;
+}
+
+void buildHeap(int *array)
+{
+    return;
+}
+
+void siftDown()
+{
+    return;
+}
+
+void heapSort(int *heap)
+{
+    buildHeap(heap);
+    int heapSize = sizeof(heap) / sizeof(heap[0]);
+    for (int i = 0; i < heapSize - 2; i++)
+    {
+        swap(&heap[0], &heap[heapSize - 1 - i]);
+        heapSize--;
+        siftDown(heap, 0, heapSize);
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int size = 0;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int *inputArray = malloc(sizeof(int) * size);
+    for (int i = 0; i < size; i++)
+    {
+        inputArray[i] = 0;
+    }
+    printf("Enter the array: ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &inputArray[i]);
+    }
+
+    heapSort(inputArray);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", inputArray[i]);
+    }
+
     return 0;
 }
