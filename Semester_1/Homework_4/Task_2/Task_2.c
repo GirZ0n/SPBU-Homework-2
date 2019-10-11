6afc3b72a971e90087b7dd16739d61ae71e34b4e
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-10-11 22:13:00 +0300

    Add database init

--- Semester_1/Homework_4/Task_2/Task_2.c
+++ Semester_1/Homework_4/Task_2/Task_2.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+const int maxSize = 256;
+
+struct Pair
+{
+    char name[256];
+    char phone[256];
+};
+
+typedef struct Pair Pair;
+
+Pair createPair(char* name, char* phone)
+{
+    Pair currentPair;
+    strcpy(currentPair.name, name);
+    strcpy(currentPair.phone, phone);
+    return currentPair;
+}
+
+int main()
+{
+    int capacity = 2;
+    int size = 0;
+    Pair* database = malloc(sizeof(Pair) * capacity);
+
+    FILE *input = fopen("Telephone_directory.txt", "r");
+    if (input != NULL)
+    {
+        char* inputString = malloc(sizeof(char) * maxSize);
+        char* name = malloc(sizeof(char) * maxSize);
+        char* phone = malloc(sizeof(char) * maxSize);
+        while (feof(input) == false)
+        {
+            if (size == capacity)
+            {
+                capacity *= 2;
+                database = realloc(database, capacity);
+            }
+
+            fgets(inputString, maxSize, input);
+
+            sscanf(inputString, "%s - %s", name, phone);
+
+            database[size] = createPair(name, phone);
+            size++;
+        }
+
+        free(inputString);
+        free(name);
+        free(phone);
+    }
+
     return 0;
 }
