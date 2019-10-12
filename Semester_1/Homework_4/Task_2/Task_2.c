4176e89ce724b595cd6b38432ef01df204426e1c
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-10-12 17:04:37 +0300

    Add case 3

--- Semester_1/Homework_4/Task_2/Task_2.c
+++ Semester_1/Homework_4/Task_2/Task_2.c
@@ -13,25 +13,20 @@ struct Pair
 
 typedef struct Pair Pair;
 
-void createPair(Pair* pair, char* name, char* phone)
+void createPair(Pair* pair, char *name, char *phone)
 {
     strcpy(pair->name, name);
     strcpy(pair->phone, phone);
 }
 
-void databaseInit(Pair* database, int* size, int* capacity)
-{
-    FILE *input = NULL;
-    input = fopen("Telephone_directory.txt", "r");
-    if (input != NULL)
-    {
-        char* inputString = malloc(sizeof(char) * maxSize);
-        char* name = malloc(sizeof(char) * maxSize);
-        char* phone = malloc(sizeof(char) * maxSize);
-        while (feof(input) == false)
-        {
-            if (size == capacity)
-            {
+void databaseInit(Pair* database, int* size, int* capacity) {
+    FILE *input = fopen("Telephone_directory.txt", "r");
+    if (input != NULL) {
+        char *inputString = malloc(sizeof(char) * maxSize);
+        char *name = malloc(sizeof(char) * maxSize);
+        char *phone = malloc(sizeof(char) * maxSize);
+        while (feof(input) == false) {
+            if (size == capacity) {
                 *capacity *= 2;
                 database = realloc(database, *capacity);
             }
@@ -49,18 +44,113 @@ void databaseInit(Pair* database, int* size, int* capacity)
     }
 }
 
+int max(int elementA, int elementB)
+{
+    if (elementA > elementB)
+    {
+        return elementA;
+    }
+
+    return elementB;
+}
+
+bool areEqual(char stringA[], char stringB[])
+{
+    int stringLength = max(strlen(stringA), strlen(stringB));
+    for (int i = 0; i < stringLength; i++)
+    {
+        if (stringA[i] != stringB[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool getNameByPhone(Pair* database, int databaseSize, char name[], char phone[])
+{
+    for (int i = 0; i < databaseSize; i++)
+    {
+        if (areEqual(database[i].phone, phone))
+        {
+            strcpy(name, database[i].name);
+            return true;
+        }
+    }
+
+    return false;
+}
+
 int main()
 {
-    int capacity = 2;
-    int size = 0;
-    Pair* database = malloc(sizeof(Pair) * capacity);
-    databaseInit(database, &size, &capacity);
+    int databaseCapacity = 2;
+    int databaseSize = 0;
+    Pair* database = malloc(sizeof(Pair) * databaseCapacity);
 
-    /*
-    for (int i = 0; i < size; i++)
+    databaseInit(database, &databaseSize, &databaseCapacity);
+
+    printf("Select an action from the suggestions below.\n");
+    printf("0 - Exit;\n");
+    printf("1 - Add telephone directory entry;\n");
+    printf("2 - Find phone by name;\n");
+    printf("3 - Find name by phone;\n");
+    printf("4 - Save current data to a file.\n\n");
+
+    char name[maxSize];
+    char phone[maxSize];
+    int action = 0;
+    while (true)
+    {
+        printf("Your action: ");
+        scanf("%d", &action);
+        switch (action)
+        {
+            case 0:
+            {
+                printf("Have a nice day! Bye :)");
+                return 0;
+            }
+            case 1:
+            {
+                break;
+            }
+            case 2:
+            {
+                break;
+            }
+            case 3:
+            {
+                printf("Enter the phone: ");
+                scanf("%s", phone);
+                if (getNameByPhone(database, databaseSize, name, phone))
+                {
+                    printf("Result: %s - %s\n", name, phone);
+                }
+                else
+                {
+                    printf("The phone number is not in the phone book.\n");
+                }
+
+                break;
+            }
+            case 4:
+            {
+                break;
+            }
+            default:
+            {
+                printf("Enter the correct action.\n");
+                break;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < databaseSize; i++)
     {
         printf("%s - %s\n", database[i].name, database[i].phone);
     }
-    */
+
     return 0;
 }
