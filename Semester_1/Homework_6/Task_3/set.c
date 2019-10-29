cd7ea81998bdac0e95a4c6c128b7741eab1650bf
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-10-29 15:28:52 +0300

    Add createSet function & createSetElement function

--- Semester_1/Homework_6/Task_3/set.c
+++ Semester_1/Homework_6/Task_3/set.c
@@ -1,1 +1,36 @@
+#include <stdlib.h>
 #include "set.h"
+
+struct Set
+{
+    struct SetElmenet* root;
+};
+
+struct SetElement
+{
+    int value;
+    struct SetElement* leftChild;
+    struct SetElement* rightChild;
+};
+
+typedef struct Set Set;
+typedef struct SetElement SetElement;
+
+Set* createSet()
+{
+    Set* set = malloc(sizeof(Set));
+    set->root = NULL;
+
+    return set;
+}
+
+SetElement* createSetElement()
+{
+    SetElement* element = malloc(sizeof(SetElement));
+    element->value = 0;
+    element->leftChild = NULL;
+    element->rightChild = NULL;
+
+    return element;
+}
+
