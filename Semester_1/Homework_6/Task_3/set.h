54402d54378e0cec28fb0cdd6e12dc64987387f2
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-10-29 15:02:24 +0300

    Wrote set.h

--- Semester_1/Homework_6/Task_3/set.h
+++ Semester_1/Homework_6/Task_3/set.h
@@ -1,4 +1,17 @@
 #ifndef SET_H
 #define SET_H
 
+#include <stdbool.h>
+
+struct Set;
+struct SetElement;
+
+struct Set* createSet();
+struct SetElement* createSetElement();
+bool isContained(int number);
+void add(int number, struct Set*);
+bool remove(int number, struct Set*);
+void printInAscendingOrder(struct Set*);
+void printInDescendingOrder(struct Set*);
+
 #endif
