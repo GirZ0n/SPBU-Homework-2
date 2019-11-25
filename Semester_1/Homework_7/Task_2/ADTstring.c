99fcfdfdf469a38952b31b39cca5a56734daf057
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-11-25 16:24:24 +0300

    Added validation for input; Renamed size to length; Changed the order of functions.

--- Semester_1/Homework_7/Task_2/ADTstring.c
+++ Semester_1/Homework_7/Task_2/ADTstring.c
@@ -5,75 +5,109 @@
 struct String
 {
     char* text;
-    int size;
+    int length;
     int capacity;
 };
 
 typedef struct String String;
 
-int getStringSize(String* string)
-{
-    return string->size;
-}
-
-int getStringCapacity(String* string)
-{
-    return string->capacity;
-}
-
 String* createString(int size)
 {
     String* newString = malloc(sizeof(String));
     newString->text = calloc(sizeof(char), size);
-    newString->size = 0;
+    newString->length = 0;
     newString->capacity = size;
-    return newString;
-}
-
-char* convertStringToCharPointer(String* string)
-{
-    return string->text;
-}
-
-String* convertStringToStructString(char* string)
-{
-    String* newString = createString(strlen(string));
-
-    int length = strlen(string);
-    for (int i = 0; i < length; i++)
-    {
-        newString->text[i] = string[i];
-    }
-    newString->size = strlen(string);
-
     return newString;
 }
 
 void resizeString(String* string, int newSize)
 {
+    if (string == NULL)
+    {
+        return;
+    }
+
     string->text = realloc(string->text, newSize);
     string->capacity = newSize;
 }
 
 void deleteString(String* string)
 {
+    if (string == NULL)
+    {
+        return;
+    }
+
     free(string->text);
     free(string);
 }
 
+int getStringLength(String *string)
+{
+   if (string == NULL)
+   {
+       return 0;
+   }
+
+   return string->length;
+}
+
+int getStringCapacity(String* string)
+{
+    if (string == NULL)
+    {
+        return 0;
+    }
+
+    return string->capacity;
+}
+
+char* convertStringToCharPointer(String* string)
+{
+    if (string == NULL)
+    {
+        return NULL;
+    }
+
+    return string->text;
+}
+
+String* convertStringToStructString(char* string)
+{
+    if (string == NULL)
+    {
+        return 0;
+    }
+
+    String* newString = createString(strlen(string));
+    int length = strlen(string);
+    for (int i = 0; i < length; i++)
+    {
+        newString->text[i] = string[i];
+    }
+    newString->length = strlen(string);
+
+    return newString;
+}
+
 bool isStringEmpty(String* string)
 {
-    return getStringSize(string) == 0;
+    return getStringLength(string) == 0;
 }
 
 bool areStringsEqual(String* stringA, String* stringB)
 {
-    if (getStringSize(stringA) != getStringSize(stringB))
+    if (stringA == NULL || stringB == NULL)
     {
         return false;
     }
 
-    int length = getStringSize(stringA);
+    if (getStringLength(stringA) != getStringLength(stringB))
+    {
+        return false;
+    }
+
+    int length = getStringLength(stringA);
     for (int i = 0; i < length; i++)
     {
         if (stringA->text[i] != stringB->text[i])
@@ -87,10 +121,15 @@ bool areStringsEqual(String* stringA, String* stringB)
 
 String* cloneString(String* string)
 {
-    String* newString = createString(getStringCapacity(string));
-    newString->size = getStringSize(string);
+    if (string == NULL)
+    {
+        return NULL;
+    }
 
-    int length = newString->size;
+    String* newString = createString(getStringCapacity(string));
+    newString->length = getStringLength(string);
+
+    int length = newString->length;
     for (int i = 0; i < length; i++)
     {
         newString->text[i] = string->text[i];
@@ -101,18 +140,28 @@ String* cloneString(String* string)
 
 void concatenateStrings(String* destination, String* source)
 {
+    if (destination == NULL || source == NULL)
+    {
+        return;
+    }
+
     resizeString(destination, getStringCapacity(destination) + getStringCapacity(source));
-    int currentLength = getStringSize(destination);
-    int newLength = getStringSize(destination) + getStringSize(source);
+    int currentLength = getStringLength(destination);
+    int newLength = getStringLength(destination) + getStringLength(source);
     for (int i = currentLength; i < newLength; i++)
     {
         destination->text[i] = source->text[i - currentLength];
     }
-    destination->size = newLength;
+    destination->length = newLength;
 }
 
 String* getSubstring(String* string, int indexOfBegin, int indexOfEnd)
 {
+    if (string == NULL)
+    {
+        return NULL;
+    }
+
     String* substring = createString(getStringCapacity(string));
 
     if (indexOfBegin < 0)
@@ -120,27 +169,27 @@ String* getSubstring(String* string, int indexOfBegin, int indexOfEnd)
         indexOfBegin = 0;
     }
 
-    if (indexOfEnd > getStringSize(string) - 1)
+    if (indexOfEnd > getStringLength(string) - 1)
     {
-        indexOfEnd = getStringSize(string) - 1;
+        indexOfEnd = getStringLength(string) - 1;
     }
 
     for (int i = 0; i < indexOfEnd - indexOfBegin + 1; i++)
     {
         substring->text[i] = string->text[indexOfBegin + i];
     }
-    substring->size = indexOfEnd - indexOfBegin + 1;
+    substring->length = indexOfEnd - indexOfBegin + 1;
     return substring;
 }
 
 void printString(String* string)
 {
-    printf("%s", string->text);
-}
+    if (string == NULL)
+    {
+        return;
+    }
 
-bool isSpecials(char input)
-{
-    return (input == ' ') || (input == '\n') || (input == '\t');
+    printf("%s", string->text);
 }
 
 struct String* getStringFromConsole()
@@ -148,16 +197,16 @@ struct String* getStringFromConsole()
     String* newString = createString(2);
     char input = ' ';
     scanf("%c", &input);
-    while (!isSpecials(input))
+    while (input != '\n')
     {
-        if (getStringSize(newString) == getStringCapacity(newString))
+        if (getStringLength(newString) == getStringCapacity(newString))
         {
             newString->capacity *= 2;
             resizeString(newString, getStringCapacity(newString));
         }
 
-        newString->text[getStringSize(newString)] = input;
-        newString->size++;
+        newString->text[getStringLength(newString)] = input;
+        newString->length++;
         scanf("%c", &input);
     }
 
@@ -167,18 +216,18 @@ struct String* getStringFromConsole()
 struct String* getStringFromFile(FILE* input)
 {
     String* newString = createString(2);
-    char inputChar = fgetc(input);
-    while (!isSpecials(inputChar) && feof(input) == false)
+    char inputChar = (char) fgetc(input);
+    while (feof(input) == false && inputChar != '\n')
     {
-        if (getStringSize(newString) == getStringCapacity(newString))
+        if (getStringLength(newString) == getStringCapacity(newString))
         {
             newString->capacity *= 2;
             resizeString(newString, getStringCapacity(newString));
         }
 
-        newString->text[getStringSize(newString)] = inputChar;
-        newString->size++;
-        inputChar = fgetc(input);
+        newString->text[getStringLength(newString)] = inputChar;
+        newString->length++;
+        inputChar = (char) fgetc(input);
     }
 
     return newString;
