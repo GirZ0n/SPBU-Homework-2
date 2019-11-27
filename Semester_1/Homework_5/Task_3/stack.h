488fd2876e535521859152d8337fa2bf92998f42
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-11-27 12:48:44 +0300

    #ifndef -> #pragma once; Added typedef; Removed Element structures.

--- Semester_1/Homework_5/Task_3/stack.h
+++ Semester_1/Homework_5/Task_3/stack.h
@@ -1,28 +1,26 @@
-#ifndef STACK_H
-#define STACK_H
+#pragma once
 
 #include <stdbool.h>
 
+typedef struct StackOfChar StackOfChar;
+typedef struct StackOfDouble StackOfDouble;
+
 // Stack of char
 struct StackOfChar;
-struct StackOfCharElement;
 
-struct StackOfChar* createStackOfChar();
-bool stackOfCharIsEmpty(struct StackOfChar* stack);
-bool pushChar(char value, struct StackOfChar* stack);
-char popChar(struct StackOfChar* stack);
-char stackOfCharTop(struct StackOfChar* stack);
+StackOfChar* createStackOfChar();
+bool stackOfCharIsEmpty(StackOfChar* stack);
+bool pushChar(char value, StackOfChar* stack);
+char popChar(StackOfChar* stack);
+char stackOfCharTop(StackOfChar* stack);
 
 
 
 // Stack of double
 struct StackOfDouble;
-struct StackOfDoubleElement;
 
-struct StackOfDouble* createStackOfDouble();
-bool stackOfDoubleIsEmpty(struct StackOfDouble* stack);
-bool pushDouble(double value, struct StackOfDouble* stack);
-double popDouble(struct StackOfDouble* stack);
-double stackOfDoubleTop(struct StackOfDouble* stack);
-
-#endif
+StackOfDouble* createStackOfDouble();
+bool stackOfDoubleIsEmpty(StackOfDouble* stack);
+bool pushDouble(double value, StackOfDouble* stack);
+double popDouble(StackOfDouble* stack);
+double stackOfDoubleTop(StackOfDouble* stack);
