22946d0e83aa28de045857aef78a3e463d454993
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-11-27 14:19:34 +0300

    Break the test function into several small ones;

--- Semester_1/Homework_7/Task_2/Task_2.c
+++ Semester_1/Homework_7/Task_2/Task_2.c
@@ -1,73 +1,133 @@
 #include <stdio.h>
 #include "ADTstring.h"
 
-void test()
+String* conversionTest(char* string)
 {
-    char* string = "2365462kn 3434fd,kwmk";
-    printf("string = %s\n", string);
-
     struct String* testString = convertStringToStructString(string);
-    printf("Conversion: length = %d, capacity = %d\n", getStringLength(testString), getStringCapacity(testString));
+    printf("Conversion: length = %d\n", getStringLength(testString));
+    return testString;
+}
 
-    resizeString(testString, 45);
-    printf("Resize: length = %d, capacity = %d\n", getStringLength(testString), getStringCapacity(testString));
-
+void cloneAndPrintTest(String* testString)
+{
     struct String* clone = cloneString(testString);
     printf("Clone and print: ");
     printString(clone);
-    printf(" (Length = %d, capacity = %d)\n", getStringLength(clone), getStringCapacity(clone));
+    printf(" (Length = %d)\n", getStringLength(clone));
+    deleteString(clone);
+}
 
+void convertAndPrintTest(String* testString)
+{
     printf("Convert and print (as char*): %s\n", convertStringToCharPointer(testString));
+}
 
+void isEmptyTest(String* testString)
+{
     printf("Is string empty: %d\n", isStringEmpty(testString));
     struct String* emptyString = convertStringToStructString("");
     printf("Is emptyString empty: %d\n", isStringEmpty(emptyString));
+    deleteString(emptyString);
+}
 
-    struct String* equalString = convertStringToStructString("2365462kn 3434fd,kwmk");
+void isEqualTest(String* testString)
+{
+    struct String* equalString = cloneString(testString);
     printf("testString == equalString: %d\n", areStringsEqual(testString, equalString));
 
-    struct String* otherString = convertStringToStructString("2365462kn 3434fd,kwrk");
+    struct String* otherString = convertStringToStructString("123");
     printf("testString == otherString: %d\n", areStringsEqual(testString, otherString));
 
+    deleteString(equalString);
+    deleteString(otherString);
+}
+
+void getSubstringTest(String* testString)
+{
     printf("testString[3:10]: ");
-    printString(getSubstring(testString, 3, 10));
+    String* substring = getSubstring(testString, 3, 10);
+    printString(substring);
+    printf(" (Length = %d)", getStringLength(substring));
     printf("\n");
 
     printf("testString[-1000:3256]: ");
-    printString(getSubstring(testString, -1000, 2235));
+    substring = getSubstring(testString, -1000, 3256);
+    printString(substring);
+    printf(" (Length = %d)", getStringLength(substring));
     printf("\n");
 
     printf("testString[435345:-235436]: ");
-    printString(getSubstring(testString, 435345, -235436));
+    substring = getSubstring(testString, 435345, -235436);
+    printString(substring);
+    printf(" (Length = %d)", getStringLength(substring));
     printf("\n");
 
+    deleteString(substring);
+}
+
+void concatenationTest(String* testString)
+{
     printf("testString + 2nmfke3: ");
     concatenateStrings(testString, convertStringToStructString("2nmfke3"));
     printString(testString);
-    printf(" (After сoncatenation: length = %d, capacity = %d)\n",
-           getStringLength(testString), getStringCapacity(testString));
+    printf(" (After сoncatenation: length = %d)\n", getStringLength(testString));
+}
 
+void getStringFromConsoleTest(String* testString)
+{
     printf("Enter string: ");
     testString = getStringFromConsole();
     printf("Get string from console and print it: ");
     printString(testString);
-    printf(" (Size = %d, capacity = %d)\n", getStringLength(testString), getStringCapacity(testString));
+    printf(" (Size = %d)\n", getStringLength(testString));
+}
 
+void getStringFromFileTest(String* testString, FILE* testFile)
+{
     printf("Get string from file and print it: ");
-    FILE* test = fopen("testFile.txt", "r");
-    testString = getStringFromFile(test);
+    testString = getStringFromFile(testFile);
     printString(testString);
-    printf(" (Size = %d, capacity = %d)\n", getStringLength(testString), getStringCapacity(testString));
+    printf(" (Size = %d)\n", getStringLength(testString));
+}
 
-    if (test != NULL)
+void test()
+{
+    char* string = "2365462kn 3434fd,kwmk";
+    printf("string = %s\n\n", string);
+
+    String* testString = conversionTest(string);
+    printf("\n");
+
+    cloneAndPrintTest(testString);
+    printf("\n");
+
+    convertAndPrintTest(testString);
+    printf("\n");
+
+    isEmptyTest(testString);
+    printf("\n");
+
+    isEqualTest(testString);
+    printf("\n");
+
+    getSubstringTest(testString);
+    printf("\n");
+
+    concatenationTest(testString);
+    printf("\n");
+
+    getStringFromConsoleTest(testString);
+    printf("\n");
+
+    FILE* testFile = fopen("testFile.txt", "r");
+    getStringFromFileTest(testString, testFile);
+
+    if (testFile != NULL)
     {
-        fclose(test);
+        fclose(testFile);
     }
 
     deleteString(testString);
-    deleteString(clone);
-    deleteString(emptyString);
-    deleteString(otherString);
 }
 
 int main()
