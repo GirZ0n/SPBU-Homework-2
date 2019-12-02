feb3e56c94689fa9701bcb5373c4230ffd723950
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-12-02 19:44:26 +0300

    Added a check for correct input

--- Semester_1/Homework_3/Task_3/Task_3.c
+++ Semester_1/Homework_3/Task_3/Task_3.c
@@ -9,21 +9,78 @@ void swap(int *array_a, int *array_b)
     *array_b = temp;
 }
 
+void cleanStdin()
+{
+    int c;
+    do
+    {
+        c = getchar();
+    }
+    while (c != EOF && c != '\n');
+}
+
+bool checkNumber(int number, bool isCorrect)
+{
+    if (!isCorrect)
+    {
+        printf("Input Error. ");
+        return false;
+    }
+
+    if (number < 0)
+    {
+        printf("Need a positive number. ");
+        return false;
+    }
+
+    return true;
+}
+
+void getSize(int* size)
+{
+    bool isCorrect = scanf("%d", size);
+    if (checkNumber(*size, isCorrect) == false)
+    {
+        do
+        {
+            cleanStdin();
+            printf("Enter the correct number: ");
+            isCorrect = scanf("%d", size);
+        }
+        while (checkNumber(*size, isCorrect) == false);
+    }
+}
+
+void getArrayElement(int* arrayElement)
+{
+    bool isCorrect = scanf("%d", arrayElement);
+    if (checkNumber(*arrayElement, isCorrect) == false)
+    {
+        do
+        {
+            cleanStdin();
+            printf("Enter the correct number: ");
+            isCorrect = scanf("%d", arrayElement);
+        }
+        while (checkNumber(*arrayElement, isCorrect) == false);
+    }
+}
+
 int main()
 {
     int size = 0;
-    printf("Enter the size of array:");
-    scanf("%d", &size);
+    printf("Enter the size of array: ");
+    getSize(&size);
 
     int *arrayOfNumbers = malloc(sizeof(int) * size);
     for (int i = 0; i < size; i++)
     {
         arrayOfNumbers[i] = 0;
     }
-    printf("Enter the array:");
+    printf("Enter the array: ");
     for (int i = 0; i < size; i++)
     {
-        scanf("%d", &arrayOfNumbers[i]);
+        getArrayElement(&arrayOfNumbers[i]);
     }
 
     int i = 0;
