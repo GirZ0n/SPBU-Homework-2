6eb0382813ccb38afe48d0dec3163c30506fb17c
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-12-08 16:42:03 +0300

    Added validation for input; Added alerts for the user about the correctness of input.

--- Semester_1/Homework_8/Task_1/Task_1.c
+++ Semester_1/Homework_8/Task_1/Task_1.c
@@ -1,11 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+void cleanStdin()
+{
+    int c;
+    do
+    {
+        c = getchar();
+    }
+    while (c != EOF && c != '\n');
+}
+
+bool checkNumberOfStudents(int inputNumber, int numberOfVariablesRead)
+{
+    if (numberOfVariablesRead != 1 || inputNumber < 3)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void getNumberOfStudents(int* numberOfUsers)
+{
+    int numberOfVariablesRead = scanf("%d", numberOfUsers);
+    while (checkNumberOfStudents(*numberOfUsers, numberOfVariablesRead) == false)
+    {
+        cleanStdin();
+        printf("Enter the correct number of students:\n");
+        numberOfVariablesRead = scanf("%d", numberOfUsers);
+    }
+    cleanStdin();
+}
+
+bool checkStudents(int studentA, int studentB, int numberOfVariablesRead, int numberOfStudents)
+{
+    if (numberOfVariablesRead != 2 || studentA < 1 || (studentB < 1 && studentB != -1))
+    {
+        return false;
+    }
+
+    if (studentA > numberOfStudents || studentB > numberOfStudents)
+    {
+        return false;
+    }
+
+    if (studentA > 3 && studentA == studentB)
+    {
+        return false;
+    }
+
+    if (studentA <= 3 && studentA != studentB)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void getStudents(int* studentA, int* studentB, int numberOfStudents)
+{
+    int numberOfVariablesRead = scanf("%d %d", studentA, studentB);
+    while (checkStudents(*studentA, *studentB, numberOfVariablesRead, numberOfStudents) == false)
+    {
+        cleanStdin();
+        printf("Enter the correct pair of students:\n");
+        numberOfVariablesRead = scanf("%d %d", studentA, studentB);
+    }
+    cleanStdin();
+}
 
 int main()
 {
-    printf("Enter the number of student: ");
+    printf("Enter the number of student (number > 0):\n");
     int numberOfStudent = 0;
-    scanf("%d", &numberOfStudent);
+    getNumberOfStudents(&numberOfStudent);
 
     // helpedCheat[i] = the number of the student whose student "i" wrote off.
     int* helpedCheat = malloc(sizeof(int) * (numberOfStudent + 1));
@@ -18,12 +88,13 @@ int main()
     helpedCheat[2] = 2;
     helpedCheat[3] = 3;
 
-    printf("Enter student number and the number of student, who helped cheat:\n");
+    printf("Enter student number (number > 0) and the number of student, "
+           "who helped cheat (number > 0 || = -1):\n");
     for (int i = 0; i < numberOfStudent; i++)
     {
         int studentA = 0;
         int studentB = 0;
-        scanf("%d %d", &studentA, &studentB);
+        getStudents(&studentA, &studentB, numberOfStudent);
 
         if (studentB == -1)
         {
