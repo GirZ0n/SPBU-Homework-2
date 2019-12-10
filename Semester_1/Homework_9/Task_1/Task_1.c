377a73517d9dabddaac4f00296abfc36c6145e07
Vlasov Ilya <ilyavlasov2011@gmail.com>
2019-12-10 14:17:29 +0300

    Moved case handling to separate functions.

--- Semester_1/Homework_9/Task_1/Task_1.c
+++ Semester_1/Homework_9/Task_1/Task_1.c
@@ -25,146 +25,190 @@ char* getStringFromConsole()
     return string;
 }
 
+int caseHandlingNumber0(char currentSymbol)
+{
+    if (currentSymbol == '+' || currentSymbol == '-')
+    {
+        return 1;
+    }
+    else if (isdigit(currentSymbol))
+    {
+        return 2;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int caseHandlingNumber1(char currentSymbol)
+{
+    if (isdigit(currentSymbol))
+    {
+        return 2;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int caseHandlingNumber2(char currentSymbol)
+{
+    if (isdigit(currentSymbol))
+    {
+        return 2;
+    }
+    else if (currentSymbol == '.')
+    {
+        return 3;
+    }
+    else if (currentSymbol == 'E')
+    {
+        return 5;
+    }
+    else if (currentSymbol == '\0')
+    {
+        return 8;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int caseHandlingNumber3(char currentSymbol)
+{
+    if (isdigit(currentSymbol))
+    {
+        return 4;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int caseHandlingNumber4(char currentSymbol)
+{
+    if (isdigit(currentSymbol))
+    {
+        return 4;
+    }
+    else if (currentSymbol == 'E')
+    {
+        return 5;
+    }
+    else if (currentSymbol == '\0')
+    {
+        return 8;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int caseHandlingNumber5(char currentSymbol)
+{
+    if (currentSymbol == '+' || currentSymbol == '-')
+    {
+        return 6;
+    }
+    else if (isdigit(currentSymbol))
+    {
+        return 7;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int caseHandlingNumber6(char currentSymbol)
+{
+    if (isdigit(currentSymbol))
+    {
+        return 7;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int caseHandlingNumber7(char currentSymbol)
+{
+    if (isdigit(currentSymbol))
+    {
+        return 7;
+    }
+    else if (currentSymbol == '\0')
+    {
+        return 8;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
 bool isRealNumber(char* input)
 {
     int iterator = 0;
     int state = 0;
     char currentSymbol = input[iterator];
-    while(true)
+    while (true)
     {
         switch (state)
         {
             case 0:
             {
-                if (currentSymbol == '+' || currentSymbol == '-')
-                {
-                    state = 1;
-                }
-                else if (isdigit(currentSymbol))
-                {
-                    state = 2;
-                }
-                else
-                {
-                    return false;
-                }
+                state = caseHandlingNumber0(currentSymbol);
                 break;
             }
             case 1:
             {
-                if (isdigit(currentSymbol))
-                {
-                    state = 2;
-                }
-                else
-                {
-                    return false;
-                }
+                state = caseHandlingNumber1(currentSymbol);
                 break;
             }
             case 2:
             {
-                if (isdigit(currentSymbol))
-                {
-                    state = 2;
-                }
-                else if (currentSymbol == '.')
-                {
-                    state = 3;
-                }
-                else if (currentSymbol == 'E')
-                {
-                    state = 5;
-                }
-                else if (currentSymbol == '\0')
-                {
-                    state = 8;
-                }
-                else
-                {
-                    return false;
-                }
+                state = caseHandlingNumber2(currentSymbol);
                 break;
             }
             case 3:
             {
-                if (isdigit(currentSymbol))
-                {
-                    state = 4;
-                }
-                else
-                {
-                    return false;
-                }
+                state = caseHandlingNumber3(currentSymbol);
                 break;
             }
             case 4:
             {
-                if (isdigit(currentSymbol))
-                {
-                    state = 4;
-                }
-                else if (currentSymbol == 'E')
-                {
-                    state = 5;
-                }
-                else if (currentSymbol == '\0')
-                {
-                    state = 8;
-                }
-                else
-                {
-                    return false;
-                }
+                state = caseHandlingNumber4(currentSymbol);
                 break;
             }
             case 5:
             {
-                if (currentSymbol == '+' || currentSymbol == '-')
-                {
-                    state = 6;
-                }
-                else if (isdigit(currentSymbol))
-                {
-                    state = 7;
-                }
-                else
-                {
-                    return false;
-                }
+                state = caseHandlingNumber5(currentSymbol);
                 break;
             }
             case 6:
             {
-                if (isdigit(currentSymbol))
-                {
-                    state = 7;
-                }
-                else
-                {
-                    return false;
-                }
+                state = caseHandlingNumber6(currentSymbol);
                 break;
             }
             case 7:
             {
-                if (isdigit(currentSymbol))
-                {
-                    state = 7;
-                }
-                else if (currentSymbol == '\0')
-                {
-                    state = 8;
-                }
-                else
-                {
-                    return false;
-                }
+                state = caseHandlingNumber7(currentSymbol);
                 break;
             }
             case 8:
             {
                 return true;
+            }
+            case -1:
+            {
+                return false;
             }
         }
         currentSymbol = input[++iterator];
