a9d46e2aab06454271f7fd57ab6bde09d0d38a5c
Vlasov Ilya <ilyavlasov2011@gmail.com>
2020-02-22 21:40:21 +0300

    Added solution for Task_2

--- Semester_2/Homework_2/Task_2/src/Task_2.kt
+++ Semester_2/Homework_2/Task_2/src/Task_2.kt
@@ -1,3 +1,30 @@
-fun main() {
+import java.util.*
 
+
+fun recursiveFactorial(number: Int): Long {
+    if (number == 0)
+        return 1
+    return number * recursiveFactorial(number - 1)
+}
+
+fun iterativeFactorial(number: Int): Long {
+    var result = 1L
+    for (i in 1..number)
+        result *= i
+    return result
+}
+
+fun getNumber(): Int {
+    val scan = Scanner(System.`in`)
+    while (!scan.hasNextInt()) {
+        println("Input Error. Try again:")
+        scan.nextLine()
+    }
+    return scan.nextInt()
+}
+
+fun main() {
+    println("Enter N:")
+    val number = getNumber()
+    println("N! = ${recursiveFactorial(number)} (Recursive), ${iterativeFactorial(number)} (Iterative)")
 }
