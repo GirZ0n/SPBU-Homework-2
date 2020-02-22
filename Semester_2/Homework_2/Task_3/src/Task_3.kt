bebe7dab56737195ca127fee9e7ffe9777209f72
Vlasov Ilya <ilyavlasov2011@gmail.com>
2020-02-22 21:56:53 +0300

    Added solution for Task_3

--- Semester_2/Homework_2/Task_3/src/Task_3.kt
+++ Semester_2/Homework_2/Task_3/src/Task_3.kt
@@ -1,3 +1,10 @@
-fun main() {
+import java.util.*
 
+fun main() {
+    val scan = Scanner(System.`in`)
+    println("Enter the string:")
+    val inputString = scan.nextLine()
+    println("Enter the substring:")
+    val substring = scan.nextLine()
+    println("The number of occurrences = ${inputString.length - inputString.replace(substring, "").length}")
 }
