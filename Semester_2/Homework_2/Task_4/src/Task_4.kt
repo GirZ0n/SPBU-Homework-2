0d5ea4d937d47ffc5554125a9cd9fdc3dd2025e5
Vlasov Ilya <ilyavlasov2011@gmail.com>
2020-02-22 22:10:56 +0300

    Added solution for Task_1.4

--- Semester_2/Homework_2/Task_4/src/Task_4.kt
+++ Semester_2/Homework_2/Task_4/src/Task_4.kt
@@ -1,3 +1,16 @@
-fun main() {
+import java.util.*
+import kotlin.system.exitProcess
 
+fun main() {
+    val scan = Scanner(System.`in`)
+    println("Enter the string:")
+    val string = scan.nextLine()
+    val stringLength = string.length
+    for (i in 0 until stringLength / 2) {
+        if (string[i] != string[stringLength - i - 1]) {
+            println("Not a palindrome.")
+            exitProcess(0)
+        }
+    }
+    println("Palindrome!")
 }
