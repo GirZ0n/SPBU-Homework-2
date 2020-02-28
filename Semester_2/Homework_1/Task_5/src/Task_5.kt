ef699a7bf0befcdf57e5c63205cb28b90aef2092
Vlasov Ilya <ilyavlasov2011@gmail.com>
2020-02-28 16:08:43 +0300

    Replaced the isEmpty function with the built-in

--- Semester_2/Homework_1/Task_5/src/Task_5.kt
+++ Semester_2/Homework_1/Task_5/src/Task_5.kt
@@ -2,25 +2,11 @@ import java.io.File
 import java.util.*
 import kotlin.system.exitProcess
 
-fun isSpecials(input: Char): Boolean {
-    return (input == ' ') || (input == '\n') || (input == '\t')
-}
-
-fun isEmpty(string: String): Boolean {
-    for (i in string.indices) {
-        if (!isSpecials(string[i]))
-            return false
-    }
-    return true
-}
-
 fun countEmptyLines(input: File): Int {
     val scan = Scanner(input)
-    var line: String
     var numberOfNonEmptyLines = 0
     while (scan.hasNextLine()) {
-        line = scan.nextLine()
-        if (!isEmpty(line))
+        if (!scan.nextLine().isBlank())
             numberOfNonEmptyLines++
     }
     return numberOfNonEmptyLines
