25987b90a37a12ef0dbc00b98575a4e7e5978f5c
Vlasov Ilya <ilyavlasov2011@gmail.com>
2020-02-29 20:46:54 +0300

    Added solution for Task_1

--- Semester_2/Homework_2/Task_1/src/Task_1.kt
+++ Semester_2/Homework_2/Task_1/src/Task_1.kt
@@ -1,3 +1,23 @@
-fun main() {
+import java.util.*
 
+fun deleteForbiddenSubstrings(inputString: String): Int {
+    var answer = 0
+    var sequenceLength = 0
+    for (i in inputString.indices) {
+        if (inputString[i] == 'x')
+            sequenceLength++
+        else {
+            answer += if (sequenceLength > 2) sequenceLength - 2 else 0
+            sequenceLength = 0
+        }
+    }
+    answer += if (sequenceLength > 2) sequenceLength - 2 else 0
+    return answer
+}
+
+fun main() {
+    println("Enter the string:")
+    val scan = Scanner(System.`in`)
+    val inputString = scan.nextLine()
+    println("Answer: ${deleteForbiddenSubstrings(inputString)}")
 }
