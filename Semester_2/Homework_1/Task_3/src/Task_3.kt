ded7a5ed282eb62bc85ec21efa02771b2a7dbd16
Vlasov Ilya <ilyavlasov2011@gmail.com>
2020-03-01 20:45:26 +0300

    Fixed no intersection recognition

--- Semester_2/Homework_1/Task_3/src/Task_3.kt
+++ Semester_2/Homework_1/Task_3/src/Task_3.kt
@@ -1,10 +1,31 @@
 import java.util.*
 
+fun areEqual(source: String, substring: String, begin: Int): Boolean {
+    for (i in substring.indices) {
+        if (source[begin + i] != substring[i])
+            return false
+    }
+    return true
+}
+
+fun getNumberOfOccurrences(source: String, substring: String): Int {
+    var answer = 0
+    val sourceLength = source.length
+    val substringLength = substring.length
+
+    for (i in 0 until sourceLength - substringLength + 1) {
+        if (areEqual(source, substring, i))
+            answer++
+    }
+
+    return answer
+}
+
 fun main() {
     val scan = Scanner(System.`in`)
     println("Enter the string:")
     val inputString = scan.nextLine()
     println("Enter the substring:")
     val substring = scan.nextLine()
-    println("The number of occurrences = ${inputString.length - inputString.replace(substring, "").length}")
+    println("The number of occurrences = ${getNumberOfOccurrences(inputString, substring)}")
 }
