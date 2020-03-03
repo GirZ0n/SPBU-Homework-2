b1f8b507a91991535847620674f6d4f3ebd195a5
Vlasov Ilya <ilyavlasov2011@gmail.com>
2020-03-03 20:48:11 +0300

    Moved isPalindrome function from main

--- Semester_2/Homework_1/Task_4/src/Task_4.kt
+++ Semester_2/Homework_1/Task_4/src/Task_4.kt
@@ -1,16 +1,17 @@
 import java.util.*
-import kotlin.system.exitProcess
+
+fun isPalindrome(inputString: String): Boolean {
+    val stringLength = inputString.length
+    for (i in 0 until stringLength / 2) {
+        if (inputString[i] != inputString[stringLength - i - 1]) {
+            return false
+        }
+    }
+    return true
+}
 
 fun main() {
     val scan = Scanner(System.`in`)
     println("Enter the string:")
-    val string = scan.nextLine()
-    val stringLength = string.length
-    for (i in 0 until stringLength / 2) {
-        if (string[i] != string[stringLength - i - 1]) {
-            println("Not a palindrome.")
-            exitProcess(0)
-        }
-    }
-    println("Palindrome!")
+    println(if (isPalindrome(scan.nextLine())) "Palindrome!" else "Not a palindrome.")
 }
