33cca19ee22786f953617a12fde421f013ad82ed
Vlasov Ilya <ilyavlasov2011@gmail.com>
2020-03-05 21:04:21 +0300

    Added tests

---
+++ Semester_2/Homework_1/Task_3/src/Task3KtTest.kt
@@ -0,0 +1,54 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+internal class Task3KtTest {
+    @Test
+    fun GetNumberOfOccurrences_SubstringIsContainedInSource1_MustWork() {
+        assertEquals(8, getNumberOfOccurrences("abababababababa", "a"))
+    }
+
+    @Test
+    fun GetNumberOfOccurrences_SubstringIsContainedInSource2_MustWork() {
+        assertEquals(7, getNumberOfOccurrences("abababababababa", "aba"))
+    }
+
+    @Test
+    fun GetNumberOfOccurrences_SubstringIsContainedInSource3_MustWork() {
+        assertEquals(1, getNumberOfOccurrences("zdkgnsdj", "zdk"))
+    }
+
+    @Test
+    fun GetNumberOfOccurrences_SubstringIsContainedInSource4_MustWork() {
+        assertEquals(1, getNumberOfOccurrences("qwerty", "qwerty"))
+    }
+
+    @Test
+    fun GetNumberOfOccurrences_SubstringIsNotContainedInSource1_MustWork() {
+        assertEquals(0, getNumberOfOccurrences("abababababababa", "q"))
+    }
+
+    @Test
+    fun GetNumberOfOccurrences_SubstringIsNotContainedInSource2_MustWork() {
+        assertEquals(0, getNumberOfOccurrences("kngbksf", "dndgjksgnjdsnjkgn"))
+    }
+
+    @Test
+    fun GetNumberOfOccurrences_SubstringIsNotContainedInSource3_MustWork() {
+        assertEquals(0, getNumberOfOccurrences("qwerty", "qwertu"))
+    }
+
+    @Test
+    fun GetNumberOfOccurrences_SubstringIsNotContainedInSource4_MustWork() {
+        assertEquals(0, getNumberOfOccurrences("abababababababa", "q"))
+    }
+
+    @Test
+    fun GetNumberOfOccurrences_TwoEmptyStrings_MustWork() {
+        assertEquals(0, getNumberOfOccurrences("ndfnsaf", "fasnfdn"))
+    }
+
+    @Test
+    fun GetNumberOfOccurrences_TwoBlankStrings_MustWork() {
+        assertEquals(6, getNumberOfOccurrences("        ", "   "))
+    }
+}
