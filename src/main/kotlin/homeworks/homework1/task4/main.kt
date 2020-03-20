65683fb8be3e578c0be126bc18a939a4ea2b4464
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-20 12:36:46 +0300

    Add solution for Task_1.4

---
+++ src/main/kotlin/homeworks/homework1/task4/main.kt
@@ -0,0 +1,17 @@
+package homeworks.homework1.task4
+
+import java.util.Scanner
+
+fun isPalindrome(inputString: String): Boolean {
+    val stringLength = inputString.length
+    for (i in 0 until stringLength / 2) {
+        if (inputString[i] != inputString[stringLength - i - 1]) return false
+    }
+    return true
+}
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    println("Enter the string:")
+    println(if (isPalindrome(scan.nextLine())) "Palindrome!" else "Not a palindrome.")
+}
