151047de2313a8baab9b80afbdc910d31588d49e
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-20 14:09:02 +0300

    Add tests for Task_2.1

---
+++ src/test/kotlin/homeworks/homework2/task1/MainTest.kt
@@ -0,0 +1,58 @@
+package homeworks.homework2.task1
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Task1Test {
+
+    @Test
+    fun CountXToBeDeleted_EmptyString_MustWork() {
+        assertEquals(0, countXToBeDeleted(""))
+    }
+
+    @Test
+    fun CountXToBeDeleted_BlankString_MustWork() {
+        assertEquals(0, countXToBeDeleted("         "))
+    }
+
+    @Test
+    fun CountXToBeDeleted_StringDoesntContainX_MustWork() {
+        assertEquals(0, countXToBeDeleted("sdnhgisd"))
+    }
+
+    @Test
+    fun CountXToBeDeleted_StringContainsX_MustWork() {
+        assertEquals(0, countXToBeDeleted("ababxababxbxbabab"))
+    }
+
+    @Test
+    fun CountXToBeDeleted_StringContainsSubstringXX_MustWork() {
+        assertEquals(0, countXToBeDeleted("ababxxababxxbxbabab"))
+    }
+
+    @Test
+    fun CountXToBeDeleted_StringConsistsOfX_MustWork() {
+        assertEquals(12, countXToBeDeleted("xxxxxxxxxxxxxx"))
+    }
+
+    @Test
+    fun CountXToBeDeleted_StringContainsSubstringXXX1_MustWork() {
+        assertEquals(5, countXToBeDeleted("xxxdsjbfxxxxdnsjnxxxx"))
+    }
+
+    @Test
+    fun CountXToBeDeleted_StringContainsSubstringXXX2_MustWork() {
+        assertEquals(8, countXToBeDeleted("aaaaaxxxxxxxxxaaxaxaxaxaxxx"))
+    }
+
+    @Test
+    fun CountXToBeDeleted_StringContainsSubstringXXX3_MustWork() {
+        assertEquals(5, countXToBeDeleted("xxxaxxxaxxxaxxxaxxxaxx"))
+    }
+
+    @Test
+    fun CountXToBeDeleted_StringContainsSubstringXXX4_MustWork() {
+        assertEquals(1, countXToBeDeleted("axaxaxaxaxaxaxaxaxaxaxaxaxaxxx"))
+    }
+}
