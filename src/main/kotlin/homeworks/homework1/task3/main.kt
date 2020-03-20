ab0b045e7df019f3486be068781ddbe57c95b423
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-20 12:28:25 +0300

    Add solution for Task_1.3

---
+++ src/main/kotlin/homeworks/homework1/task3/main.kt
@@ -0,0 +1,25 @@
+package homeworks.homework1.task3
+
+import java.util.Scanner
+
+fun getNumberOfOccurrences(source: String, substring: String): Int {
+    var answer = 0
+    val sourceLength = source.length
+    val substringLength = substring.length
+
+    for (i in 0 until sourceLength - substringLength + 1) {
+        if (source.slice(i until i + substringLength) == substring)
+            answer++
+    }
+
+    return if (substringLength == 0) 0 else answer
+}
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    println("Enter the string:")
+    val inputString = scan.nextLine()
+    println("Enter the substring:")
+    val substring = scan.nextLine()
+    println("The number of occurrences = ${getNumberOfOccurrences(inputString, substring)}")
+}
