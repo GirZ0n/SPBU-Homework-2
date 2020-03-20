4ff51f104bebdd74f3d5bc6a3a44b7c733be2758
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-20 16:42:02 +0300

    Add exception throw for Test_1.1

--- src/main/kotlin/tests/test1/priorityQueue.kt
+++ src/main/kotlin/tests/test1/priorityQueue.kt
@@ -13,14 +13,19 @@ class PriorityQueue<T> {
     }
 
     fun dequeue() {
-        if (list.isNotEmpty()) list.removeAt(0)
+        if (list.isNotEmpty()) list.removeAt(0) else
+            throw ArrayIndexOutOfBoundsException("Priority Queue is empty\n")
     }
 
-    fun peek(): T? {
-        return if (list.isNotEmpty()) list[0].second else null
+    fun peek(): T {
+        return if (list.isNotEmpty()) list[0].second else
+            throw ArrayIndexOutOfBoundsException("Priority Queue is empty\n")
     }
 
     fun print() {
-        println(if (list.isNotEmpty()) list.joinToString(" ") else "Queue is empty")
+        println(
+            if (list.isNotEmpty()) list.joinToString(" ") else throw
+                ArrayIndexOutOfBoundsException("Priority Queue is empty\n")
+        )
     }
 }
