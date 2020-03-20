8ead473dd9d31840b846b17ceb26f84a448d5871
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-20 14:09:10 +0300

    Add solution for Task_2.1

---
+++ src/main/kotlin/homeworks/homework2/task1/main.kt
@@ -0,0 +1,27 @@
+package homeworks.homework2.task1
+
+import java.util.Scanner
+
+fun countXToBeDeleted(inputString: String): Int {
+    var answer = 0
+    var sequenceLength = 0
+    for (element in inputString) {
+        if (element == 'x') sequenceLength++
+        else {
+            answer += if (sequenceLength > 2) sequenceLength - 2 else 0
+            sequenceLength = 0
+        }
+    }
+    answer += if (sequenceLength > 2) sequenceLength - 2 else 0
+    return answer
+}
+
+fun main() {
+    println("Enter the string:")
+    val scan = Scanner(System.`in`)
+    val inputString = scan.nextLine()
+    println(
+        "You need to remove ${countXToBeDeleted(inputString)} symbols " +
+                "so that the string doesn't contain the substring \"xxx\""
+    )
+}
