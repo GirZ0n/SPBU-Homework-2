add8083f7ace6dd21805beab62ed9b40860f68e0
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-22 21:20:05 +0300

    Added a lot of checks for various exceptions

--- src/main/kotlin/homeworks/homework1/task1/main.kt
+++ src/main/kotlin/homeworks/homework1/task1/main.kt
@@ -1,51 +1,73 @@
 package homeworks.homework1.task1
 
-import java.util.Scanner
+import java.util.*
 import kotlin.system.exitProcess
 
 fun getNumber(): Int {
     val scan = Scanner(System.`in`)
     val number = scan.nextInt()
-    if (number < 1) {
-        println("Error. The number must be greater than 0")
-        exitProcess(0)
-    }
+    require(number > 0) { "The number must be greater than 0" }
     return number
 }
 
-fun getArray(size: Int): IntArray {
-    val scan = Scanner(System.`in`)
-    val array = IntArray(size) { 0 }
-    for (i in array.indices) {
-        array[i] = scan.nextInt()
-    }
-    return array
-}
-
-fun reverse(array: IntArray, indexOfBegin: Int, indexOfEnd: Int) {
+fun reverse(array: List<Int>, indexOfBegin: Int, indexOfEnd: Int): List<Int> {
     val begin = if (indexOfBegin < 0) 0 else indexOfBegin
     val end = if (indexOfEnd > array.size) array.size else indexOfEnd
 
-    val lengthOfSegment = end - begin
-    for (i in 0 until lengthOfSegment / 2) {
-        val temp = array[begin + i]
-        array[begin + i] = array[end - i - 1]
-        array[end - i - 1] = temp
-    }
+    val beginSegment = array.subList(0, begin)
+    val reversedSegment = array.subList(begin, end).reversed()
+    val endSegment = array.subList(end, array.size)
+
+    return beginSegment + reversedSegment + endSegment
+}
+
+fun swapStartAndEnd(array: List<Int>, beginLength: Int, endLength: Int): List<Int> {
+    require(beginLength + endLength == array.size) { "The array should be long: ${beginLength + endLength}" }
+    var answer = array
+    answer = reverse(answer, 0, beginLength)
+    answer = reverse(answer, beginLength, beginLength + endLength)
+    answer = reverse(answer, 0, beginLength + endLength)
+    return answer
 }
 
 fun main() {
-    println("Enter the value of N:")
-    val numberN = getNumber()
-    println("Enter the value of M:")
-    val numberM = getNumber()
-    println("Enter the array:")
-    val array = getArray(numberN + numberM)
+    val beginSegmentLength: Int
+    val endSegmentLength: Int
+    try {
+        println("Enter the value of N:")
+        beginSegmentLength = getNumber()
+        println("Enter the value of M:")
+        endSegmentLength = getNumber()
+    } catch (exception: IllegalArgumentException) {
+        println(exception.message)
+        exitProcess(0)
+    } catch (exception: InputMismatchException) {
+        println("You must enter a number")
+        exitProcess(0)
+    } catch (exception: NoSuchElementException) {
+        println("You must enter a number")
+        exitProcess(0)
+    }
 
-    reverse(array, 0, numberN)
-    reverse(array, numberN, numberN + numberM)
-    reverse(array, 0, numberN + numberM)
+    var array: List<Int>
+    try {
+        println("Enter the array:")
+        array = readLine()!!.split(' ').map { it.toInt() }
+    } catch (exception: NumberFormatException) {
+        println("You need to enter an array of integers")
+        exitProcess(0)
+    } catch (exception: KotlinNullPointerException) {
+        println("You need to enter an array of integers")
+        exitProcess(0)
+    }
+
+    try {
+        array = swapStartAndEnd(array, beginSegmentLength, endSegmentLength)
+    } catch (exception: IllegalArgumentException) {
+        println(exception.message)
+        exitProcess(0)
+    }
 
     println("Result:")
-    print(array.joinToString(" "))
+    print(array.subList(0, beginSegmentLength + endSegmentLength).joinToString(" "))
 }
