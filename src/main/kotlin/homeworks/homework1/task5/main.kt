55f63b09d315585233d588cf8b8320993eaf473a
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-25 22:45:52 +0300

    Now fun countNonEmptyLines throws exception

--- src/main/kotlin/homeworks/homework1/task5/main.kt
+++ src/main/kotlin/homeworks/homework1/task5/main.kt
@@ -1,9 +1,12 @@
 package homeworks.homework1.task5
 
 import java.io.File
+import java.io.FileNotFoundException
 import java.util.Scanner
 
 fun countNonEmptyLines(input: File): Int {
+    if (!input.exists()) throw FileNotFoundException("Can't open the file")
+
     val scan = Scanner(input)
     var numberOfNonEmptyLines = 0
     while (scan.hasNextLine()) {
@@ -14,8 +17,9 @@ fun countNonEmptyLines(input: File): Int {
 
 fun main() {
     val input = File("./src/main/kotlin/homeworks/homework1/task5/input.txt")
-    if (!input.exists()) {
-        println("Can't open the file")
-        return
-    } else println("The number of non-empty lines = ${countNonEmptyLines(input)}")
+    try {
+        println("The number of non-empty lines = ${countNonEmptyLines(input)}")
+    } catch (exception: FileNotFoundException) {
+        println(exception)
+    }
 }
