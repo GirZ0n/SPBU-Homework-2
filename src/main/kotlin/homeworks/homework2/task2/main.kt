972fb76e7a336e319c96985c66f8f9d3c95acd5d
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-26 11:01:07 +0300

    Added a new array input method with exception handling

--- src/main/kotlin/homeworks/homework2/task2/main.kt
+++ src/main/kotlin/homeworks/homework2/task2/main.kt
@@ -1,20 +1,31 @@
 package homeworks.homework2.task2
 
-import java.util.Scanner
+import kotlin.system.exitProcess
 
-fun deleteAllLeftEntries(inputArray: Array<Any>): Array<Any> {
-    return inputArray.reversed().toSet().toTypedArray().reversed().toTypedArray()
+fun deleteAllLeftEntries(inputArray: List<Any>): List<Any> {
+    return inputArray.reversed().toSet().toTypedArray().reversed()
+}
+
+fun getArray(): List<Int> {
+    val array: List<Int>
+
+    try {
+        println("Enter the array:")
+        array = readLine()!!.split(' ').map { it.toInt() }
+    } catch (exception: NumberFormatException) {
+        println("You need to enter an array of integers")
+        exitProcess(0)
+    } catch (exception: KotlinNullPointerException) {
+        println("You need to enter an array of integers")
+        exitProcess(0)
+    }
+
+    return array
 }
 
 fun main() {
-    val scan = Scanner(System.`in`)
-    println("Enter the size of the array:")
-    var array = Array<Any>(scan.nextInt()) { 0 }
     println("Attention! The program works with any type of data, but reads only integers.\n")
-    println("Enter the array:")
-    for (i in array.indices)
-        array[i] = scan.nextInt()
-
+    var array = getArray() as List<Any>
     array = deleteAllLeftEntries(array)
     println("Answer:")
     println(array.joinToString(" "))
