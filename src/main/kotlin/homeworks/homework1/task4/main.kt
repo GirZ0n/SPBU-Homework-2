a162b564acba4382b6a2c36563bb2e69f946a0af
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-28 14:32:44 +0300

    Now fun isPalindrome throw IllegalArgumentException

--- src/main/kotlin/homeworks/homework1/task4/main.kt
+++ src/main/kotlin/homeworks/homework1/task4/main.kt
@@ -1,8 +1,11 @@
 package homeworks.homework1.task4
 
+import java.lang.IllegalArgumentException
 import java.util.Scanner
 
 fun isPalindrome(inputString: String): Boolean {
+    require(inputString.isNotEmpty()) { "The inputString mustn't be empty" }
+
     val stringLength = inputString.length
     for (i in 0 until stringLength / 2) {
         if (inputString[i] != inputString[stringLength - i - 1]) return false
@@ -13,5 +16,9 @@ fun isPalindrome(inputString: String): Boolean {
 fun main() {
     val scan = Scanner(System.`in`)
     println("Enter the string:")
-    println(if (isPalindrome(scan.nextLine())) "Palindrome!" else "Not a palindrome.")
+    try {
+        println(if (isPalindrome(scan.nextLine())) "Palindrome!" else "Not a palindrome.")
+    } catch (exception: IllegalArgumentException) {
+        println(exception)
+    }
 }
