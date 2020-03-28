02ee054c434d4a3667634531bf5af958d9b96ccd
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-28 23:10:48 +0300

    Code refactoring

--- src/main/kotlin/homeworks/homework3/task1/AVLSet.kt
+++ src/main/kotlin/homeworks/homework3/task1/AVLSet.kt
@@ -1,24 +1,47 @@
 package homeworks.homework3.task1
 
-private class Node<K, V>(var key: K, var value: V) {
-    var leftChild: Node<K, V>? = null
-    var rightChild: Node<K, V>? = null
-
-    fun getHeight(): Int {
-        TODO()
-    }
-
-    fun getBalanceFactor(): Int {
-        TODO()
-    }
-
-    fun updateHeight() {
-        TODO()
-    }
-}
-
 class AVLSet<K, V> {
     private var root: Node<K, V>? = null
+    private val balanceTree = AVLBalance<K, V>()
+
+    private class AVLBalance<K, V> {
+        private fun rotateLeft(root: Node<K, V>): Node<K, V>? {
+            TODO()
+        }
+
+        private fun rotateRight(root: Node<K, V>): Node<K, V>? {
+            TODO()
+        }
+
+        private fun balance(root: Node<K, V>): Node<K, V>? {
+            TODO()
+        }
+
+        private fun getMinimumNodeFromRightSubtree(root: Node<K, V>): Node<K, V> {
+            TODO()
+        }
+
+        private fun removeMinimumNodeFromRightSubtree(root: Node<K, V>): Node<K, V> {
+            TODO()
+        }
+    }
+
+    private class Node<K, V>(var key: K, var value: V) {
+        var leftChild: Node<K, V>? = null
+        var rightChild: Node<K, V>? = null
+
+        fun getHeight(): Int {
+            TODO()
+        }
+
+        fun getBalanceFactor(): Int {
+            TODO()
+        }
+
+        fun updateHeight() {
+            TODO()
+        }
+    }
 
     fun isKeyContained(key: K): Boolean {
         TODO()
@@ -49,26 +72,6 @@ class AVLSet<K, V> {
     }
 
     fun printSet() {
-        TODO()
-    }
-
-    private fun rotateLeft(): Node<K, V>? {
-        TODO()
-    }
-
-    private fun rotateRight(): Node<K, V>? {
-        TODO()
-    }
-
-    private fun balance(): Node<K, V>? {
-        TODO()
-    }
-
-    private fun getMinimumNodeFromRightSubtree(): Node<K, V> {
-        TODO()
-    }
-
-    private fun removeMinimumNodeFromRightSubtree(): Node<K, V> {
         TODO()
     }
 }
