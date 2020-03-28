8bde03db4493b565e0038e03d53e0e1e846568b2
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-28 13:30:33 +0300

    Change assertEquals to assert and assertFalse

--- src/test/kotlin/homeworks/homework1/task4/MainTest.kt
+++ src/test/kotlin/homeworks/homework1/task4/MainTest.kt
@@ -8,56 +8,56 @@ internal class Task4Test {
 
     @Test
     fun IsPalindrome_PalindromeString1_MustWork() {
-        assertEquals(true, isPalindrome("abba"))
+        assert(isPalindrome("abba"))
     }
 
     @Test
     fun IsPalindrome_PalindromeString2_MustWork() {
-        assertEquals(true, isPalindrome("saippuakivikauppias"))
+        assert(isPalindrome("saippuakivikauppias"))
     }
 
     @Test
     fun IsPalindrome_PalindromeString3_MustWork() {
-        assertEquals(true, isPalindrome("abcdefedcba"))
+        assert(isPalindrome("abcdefedcba"))
     }
 
     @Test
     fun IsPalindrome_NotPalindromeString1_MustWork() {
-        assertEquals(false, isPalindrome("qwe"))
+        assertFalse(isPalindrome("qwe"))
     }
 
     @Test
     fun IsPalindrome_NotPalindromeString2_MustWork() {
-        assertEquals(false, isPalindrome("dkngjsngio"))
+        assertFalse(isPalindrome("dkngjsngio"))
     }
 
     @Test
     fun IsPalindrome_NotPalindromeString3_MustWork() {
-        assertEquals(false, isPalindrome("oooooooooooooooooouooooooooooo"))
+        assertFalse(isPalindrome("oooooooooooooooooouooooooooooo"))
     }
 
     @Test
     fun IsPalindrome_SingleCharacterString_MustWork() {
-        assertEquals(true, isPalindrome("aaaaaaaaaaa"))
+        assert(isPalindrome("aaaaaaaaaaa"))
     }
 
     @Test
     fun IsPalindrome_EmptyString_MustWork() {
-        assertEquals(true, isPalindrome(""))
+        assert(isPalindrome(""))
     }
 
     @Test
     fun IsPalindrome_BlankString_MustWork() {
-        assertEquals(true, isPalindrome("              "))
+        assert(isPalindrome("              "))
     }
 
     @Test
     fun IsPalindrome_PalindromeOfEvenLength_MustWork() {
-        assertEquals(true, isPalindrome("abccba"))
+        assert(isPalindrome("abccba"))
     }
 
     @Test
     fun IsPalindrome_PalindromeOfOddLength_MustWork() {
-        assertEquals(true, isPalindrome("abcba"))
+        assert(isPalindrome("abcba"))
     }
 }
