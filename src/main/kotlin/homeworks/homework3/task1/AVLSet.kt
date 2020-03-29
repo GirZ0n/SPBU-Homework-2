680436ea7011dc817f913ec9cd3568ed2e6adf8b
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-03-29 19:21:37 +0300

    Now AVLSet сan add pairs and print keys

--- src/main/kotlin/homeworks/homework3/task1/AVLSet.kt
+++ src/main/kotlin/homeworks/homework3/task1/AVLSet.kt
@@ -2,94 +2,13 @@ package homeworks.homework3.task1
 
 class AVLSet<K, V> where K : Comparable<K>, V : Comparable<V> {
     private var root: Node<K, V>? = null
+    private var size = 0
 
+    fun printKeys() = root?.printKeys() ?: print("Null")
 
-    private class Node<K, V>(var key: K, var value: V) where K : Comparable<K>, V : Comparable<V> {
-        var balanceFactor: Int = 0
-            private set
-        var leftChild: Node<K, V>? = null
-        var rightChild: Node<K, V>? = null
-        private val balanceTree = AVLBalance<K, V>()
+    fun containsKey(key: K): Boolean {
+        if (root == null) return false
 
-        private class AVLBalance<K, V> where K : Comparable<K>, V : Comparable<V> {
-            private fun rotateLeft(root: Node<K, V>?): Node<K, V>? {
-                if (root == null) return null
-
-                val pivot = root.rightChild
-                root.rightChild = pivot?.leftChild
-                pivot?.leftChild = root
-                root.updateBalanceFactor()
-                pivot?.updateBalanceFactor()
-
-                return pivot
-            }
-
-            private fun rotateRight(root: Node<K, V>?): Node<K, V>? {
-                if (root == null) return null
-
-                val pivot = root.leftChild
-                root.leftChild = pivot?.rightChild
-                pivot?.rightChild = root
-                root.updateBalanceFactor()
-                pivot?.updateBalanceFactor()
-
-                return pivot
-            }
-
-            fun balance(root: Node<K, V>): Node<K, V>? {
-                root.updateBalanceFactor()
-                val rightChildBalanceFactor = root.rightChild?.balanceFactor ?: -1
-                val leftChildBalanceFactor = root.leftChild?.balanceFactor ?: -1
-
-                if (root.balanceFactor == 2) {
-                    if (rightChildBalanceFactor < 0) {
-                        root.rightChild = rotateRight(root.rightChild)
-                    }
-                    return rotateLeft(root)
-                }
-
-                if (root.balanceFactor == -2) {
-                    if (leftChildBalanceFactor > 0) {
-                        root.leftChild = rotateLeft(root.leftChild);
-                    }
-                    return rotateRight(root);
-                }
-
-                return root
-            }
-        }
-
-        private fun getHeight(): Int {
-            val leftChildHeight = this.leftChild?.getHeight() ?: -1
-            val rightChildHeight = this.rightChild?.getHeight() ?: -1
-            return 1 + maxOf(leftChildHeight, rightChildHeight)
-        }
-
-        fun updateBalanceFactor() {
-            val leftChildHeight = leftChild?.getHeight() ?: -1
-            val rightChildHeight = rightChild?.getHeight() ?: -1
-            balanceFactor = rightChildHeight - leftChildHeight
-        }
-
-        fun add(key: K, value: V): Node<K, V>? {
-            if (this.key > key) {
-                this.leftChild?.add(key, value) ?: also { it.leftChild = Node(key, value) }
-            } else if (this.key < key) {
-                this.rightChild?.add(key, value) ?: also { it.rightChild = Node(key, value) }
-            }
-
-            return balanceTree.balance(this)
-        }
-
-        fun printKeys() {
-            print("( ${this.key} ")
-            this.leftChild?.printKeys() ?: run {print(" Null")}
-            this.rightChild?.printKeys() ?: run {print(" Null")}
-            print(") ")
-        }
-    }
-
-    fun isKeyContained(key: K): Boolean {
         var current = root
         while (current != null) {
             current = when {
@@ -98,42 +17,92 @@ class AVLSet<K, V> where K : Comparable<K>, V : Comparable<V> {
                 else -> current.rightChild
             }
         }
+
         return false
     }
 
-    fun isValueContained(value: V): Boolean {
+    fun containsValue(value: V): Boolean {
         TODO()
     }
 
     fun add(key: K, value: V) {
-        if (root == null) root = Node(key, value)
-
-        if (!isKeyContained(key)) root = root?.add(key, value)
+        if (!containsKey(key)) {
+            root = root?.add(key, value) ?: Node(key, value)
+        }
     }
 
-    fun changeValue(key: K, value: V) {
-        TODO()
-    }
+    class Node<K, V>(val key: K, var value: V) where K : Comparable<K> {
+        private var height = 0
+        var leftChild: Node<K, V>? = null
+        var rightChild: Node<K, V>? = null
 
-    fun removeKey(key: K) {
-        TODO()
-    }
+        private fun updateHeight() {
+            val leftChildHeight = leftChild?.height ?: -1
+            val rightChildHeight = rightChild?.height ?: -1
+            this.height = maxOf(leftChildHeight, rightChildHeight) + 1
+        }
 
-    fun removeValue(value: V) {
-        TODO()
-    }
+        fun getBalanceFactor(): Int {
+            val leftChildHeight = leftChild?.height ?: -1
+            val rightChildHeight = rightChild?.height ?: -1
+            return rightChildHeight - leftChildHeight
+        }
 
+        fun printKeys() {
+            print("( $key ")
+            leftChild?.printKeys() ?: print(" Null ")
+            rightChild?.printKeys() ?: print(" Null ")
+            print(") ")
+        }
 
-    private fun getMinimumNodeFromRightSubtree(root: Node<K, V>): Node<K, V> {
-        TODO()
-    }
+        fun add(key: K, value: V): Node<K, V>? {
+            if (this.key > key) {
+                leftChild = leftChild?.add(key, value) ?: Node(key, value)
+            } else if (this.key < key) {
+                rightChild = rightChild?.add(key, value) ?: Node(key, value)
+            }
 
-    private fun removeMinimumNodeFromRightSubtree(root: Node<K, V>): Node<K, V> {
-        TODO()
-    }
+            return balance()
+        }
 
+        fun rotateRight(): Node<K, V>? {
+            val pivot = leftChild
+            leftChild = pivot?.rightChild
+            pivot?.rightChild = this
+            updateHeight()
+            pivot?.updateHeight()
+            return pivot
+        }
 
-    fun printKeys() {
-        root?.printKeys() ?: run { println("NULL") }
+        fun rotateLeft(): Node<K, V>? {
+            val pivot = rightChild
+            rightChild = pivot?.leftChild
+            pivot?.leftChild = this
+            updateHeight()
+            pivot?.updateHeight()
+            return pivot
+        }
+
+        fun balance(): Node<K, V>? {
+            updateHeight()
+            val rightChildBalanceFactor = rightChild?.getBalanceFactor() ?: -1
+            val leftChildBalanceFactor = leftChild?.getBalanceFactor() ?: -1
+
+            if (getBalanceFactor() == 2) {
+                if (rightChildBalanceFactor < 0) {
+                    rightChild = rightChild?.rotateRight()
+                }
+                return rotateLeft()
+            }
+
+            if (getBalanceFactor() == -2) {
+                if (leftChildBalanceFactor > 0) {
+                    leftChild = leftChild?.rotateLeft()
+                }
+                return rotateRight()
+            }
+
+            return this
+        }
     }
 }
