fd6a2dae33d559f114298dfc2b1cfc723dd82c29
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-04-20 19:48:34 +0300

    Fixed Detekt issue

--- src/main/kotlin/homeworks/homework4/task1/main.kt
+++ src/main/kotlin/homeworks/homework4/task1/main.kt
@@ -35,51 +35,56 @@ fun printHelp() {
 
 fun interactWithTable(hashTable: HashTable<String, Int>) {
     var input: String
-    loop@
     while (true) {
         print("> ")
         input = readLine() ?: "exit"
         when (input.toLowerCase()) {
             Commands.HELP.code -> printHelp()
-            Commands.ADD.code -> {
-                println("Enter the key (string):")
-                val key = readLine()
-                println("Enter the value (integer):")
-                val value = readLine()?.toInt()
-                hashTable.add(key, value)
-            }
-            Commands.REMOVE.code -> {
-                println("Enter the key (string):")
-                val key = readLine()
-                hashTable.remove(key)
-            }
-            Commands.FIND.code -> {
-                println("Enter the key (string):")
-                val key = readLine()
-                println("$key: ${hashTable.find(key)}")
-            }
-            Commands.CHANGE_HASH_FUNCTION.code -> {
-                println("What hash function do you want to use?")
-                println("0: simple hash function, 1: polynomial hash function")
-                print("hash function > ")
-                when (readLine()?.toInt() ?: 1) {
-                    0 -> {
-                        hashTable.changeHashFunction(::simpleHashFunction)
-                        println("You have selected a simple hash function")
-                    }
-                    1 -> {
-                        hashTable.changeHashFunction(::polynomialHashFunction)
-                        println("You have selected a polynomial hash function")
-                    }
-                }
-            }
+            Commands.ADD.code -> addCodeProcessing(hashTable)
+            Commands.REMOVE.code -> removeCodeProcessing(hashTable)
+            Commands.FIND.code -> findCodeProcessing(hashTable)
+            Commands.CHANGE_HASH_FUNCTION.code -> changeHashFunctionCodeProcessing(hashTable)
             Commands.PRINT_STATISTICS.code -> hashTable.printStatistics()
             Commands.EXIT.code -> return
-            else -> {
-                println("Type `${Commands.HELP.code}` to see help list")
-                continue@loop
-            }
+            else -> println("Type `${Commands.HELP.code}` to see help list")
         }
+    }
+}
+
+fun addCodeProcessing(hashTable: HashTable<String, Int>) {
+    println("Enter the key (string):")
+    val key = readLine()
+    println("Enter the value (integer):")
+    val value = readLine()?.toInt()
+    hashTable.add(key, value)
+}
+
+fun removeCodeProcessing(hashTable: HashTable<String, Int>) {
+    println("Enter the key (string):")
+    val key = readLine()
+    hashTable.remove(key)
+}
+
+fun findCodeProcessing(hashTable: HashTable<String, Int>) {
+    println("Enter the key (string):")
+    val key = readLine()
+    println("$key: ${hashTable.find(key)}")
+}
+
+fun changeHashFunctionCodeProcessing(hashTable: HashTable<String, Int>) {
+    println("What hash function do you want to use?")
+    println("0: simple hash function, 1: polynomial hash function")
+    print("hash function > ")
+    when (readLine()?.toInt()) {
+        0 -> {
+            hashTable.changeHashFunction(::simpleHashFunction)
+            println("You have selected a simple hash function")
+        }
+        1 -> {
+            hashTable.changeHashFunction(::polynomialHashFunction)
+            println("You have selected a polynomial hash function")
+        }
+        else -> return
     }
 }
 
