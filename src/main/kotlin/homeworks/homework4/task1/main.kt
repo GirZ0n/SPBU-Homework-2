a61fb4f5f98b92d145e562e263bf0577b75ac18e
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-04-20 19:36:46 +0300

    Added interactWithTable function

--- src/main/kotlin/homeworks/homework4/task1/main.kt
+++ src/main/kotlin/homeworks/homework4/task1/main.kt
@@ -1,21 +1,25 @@
 package homeworks.homework4.task1
 
+import kotlin.math.pow
+
+const val INIT_SIZE = 1024
+const val PRIME_NUMBER = 1073676287.0
+
 fun main() {
     printHelp()
-    while (true) {
-        return
-    }
+    val hashTable = HashTable<String, Int>(INIT_SIZE, ::polynomialHashFunction)
+    interactWithTable(hashTable)
 }
 
-enum class Commands(val code: Int) {
-    HELP(0),
-    ADD(1),
-    REMOVE(2),
-    FIND(3),
-    CHANGE_HASH_FUNCTION(4),
-    PRINT_STATISTICS(5),
-    IMPORT(6),
-    EXIT(7);
+enum class Commands(val code: String) {
+    HELP("help"),
+    ADD("add"),
+    REMOVE("remove"),
+    FIND("find"),
+    CHANGE_HASH_FUNCTION("change"),
+    PRINT_STATISTICS("stats"),
+    IMPORT("import"),
+    EXIT("exit");
 }
 
 fun printHelp() {
@@ -27,4 +31,72 @@ fun printHelp() {
     println("Type `${Commands.PRINT_STATISTICS.code}` to write statistics;")
     println("Type `${Commands.IMPORT.code}` to import a hash table from a file;")
     println("Type `${Commands.EXIT.code}` to complete the program.")
+}
+
+fun interactWithTable(hashTable: HashTable<String, Int>) {
+    var input: String
+    loop@
+    while (true) {
+        print("> ")
+        input = readLine() ?: "exit"
+        when (input.toLowerCase()) {
+            Commands.HELP.code -> printHelp()
+            Commands.ADD.code -> {
+                println("Enter the key (string):")
+                val key = readLine()
+                println("Enter the value (integer):")
+                val value = readLine()?.toInt()
+                hashTable.add(key, value)
+            }
+            Commands.REMOVE.code -> {
+                println("Enter the key (string):")
+                val key = readLine()
+                hashTable.remove(key)
+            }
+            Commands.FIND.code -> {
+                println("Enter the key (string):")
+                val key = readLine()
+                println("$key: ${hashTable.find(key)}")
+            }
+            Commands.CHANGE_HASH_FUNCTION.code -> {
+                println("What hash function do you want to use?")
+                println("0: simple hash function, 1: polynomial hash function")
+                print("hash function > ")
+                when (readLine()?.toInt() ?: 1) {
+                    0 -> {
+                        hashTable.changeHashFunction(::simpleHashFunction)
+                        println("You have selected a simple hash function")
+                    }
+                    1 -> {
+                        hashTable.changeHashFunction(::polynomialHashFunction)
+                        println("You have selected a polynomial hash function")
+                    }
+                }
+            }
+            Commands.PRINT_STATISTICS.code -> hashTable.printStatistics()
+            Commands.EXIT.code -> return
+            else -> {
+                println("Type `${Commands.HELP.code}` to see help list")
+                continue@loop
+            }
+        }
+    }
+}
+
+fun simpleHashFunction(inputString: String): Int {
+    var hash = 0
+    for (symbol in inputString) {
+        hash += symbol.toInt()
+    }
+    return hash
+}
+
+fun polynomialHashFunction(inputString: String): Int {
+    var hash = 0
+    var index = 0
+    for (symbol in inputString) {
+        hash += (symbol.toInt() * PRIME_NUMBER.pow(index)).toInt()
+        index++
+    }
+    return hash
 }
