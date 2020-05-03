c575b5229fd38c83b8da8133b6462c8665c5813b
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-05-03 19:01:24 +0300

    Added Operator class

--- src/main/kotlin/homeworks/homework4/task2/Operator.kt
+++ src/main/kotlin/homeworks/homework4/task2/Operator.kt
@@ -3,13 +3,13 @@ package homeworks.homework4.task2
 class Operator(val sign: Char) : Node() {
     override fun print() = print(sign)
     override fun calculate(): Int {
-        checkNotNull(leftChild) { "The left operand is missing" }
-        checkNotNull(rightChild) { "The right operand is missing" }
+        val leftResult = leftChild?.calculate() ?: throw IllegalStateException("The left operand is missing")
+        val rightResult = rightChild?.calculate() ?: throw IllegalStateException("The right operand is missing")
         return when (sign) {
-            '+' -> leftChild.calculate() + rightChild.calculate()
-            '-' -> leftChild.calculate() - rightChild.calculate()
-            '*' -> leftChild.calculate() * rightChild.calculate()
-            '/' -> leftChild.calculate() / rightChild.calculate()
+            '+' -> leftResult + rightResult
+            '-' -> leftResult - rightResult
+            '*' -> leftResult * rightResult
+            '/' -> leftResult / rightResult
             else -> throw IllegalStateException("Operator sign cannot be recognized")
         }
     }
