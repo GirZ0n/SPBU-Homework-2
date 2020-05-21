aefa9a1c6fe2fce9f0017ac22861c84ed9173b0f
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-05-21 14:27:48 +0300

    Change STAGE_HEIGHT and STAGE_WIDTH to 400.0

--- src/main/kotlin/homeworks/homework7/task2/TicTacToeApp.kt
+++ src/main/kotlin/homeworks/homework7/task2/TicTacToeApp.kt
@@ -4,10 +4,10 @@ import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.launch
 
-private const val STAGE_HEIGHT = 300.0
-private const val STAGE_WIDTH = 300.0
+private const val STAGE_HEIGHT = 400.0
+private const val STAGE_WIDTH = 400.0
 
-class TicTacToeApp : App(Menu::class) {
+class TicTacToeApp : App(Menu::class, MenuStyle::class) {
     override fun start(stage: Stage) {
         stage.height = STAGE_HEIGHT
         stage.width = STAGE_WIDTH
