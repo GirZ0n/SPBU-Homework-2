560c3acdd74d7112b09d8c8f2f376c2ef85f73a7
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-05-22 18:40:30 +0300

    Style fix

--- src/main/kotlin/tests/test2/Menu.kt
+++ src/main/kotlin/tests/test2/Menu.kt
@@ -1,11 +1,12 @@
 package tests.test2
 
 import javafx.beans.property.SimpleIntegerProperty
+import javafx.geometry.Pos
 import tornadofx.*
 
 class Menu : View() {
     var size = SimpleIntegerProperty()
-    override val root = vbox {
+    override val root = vbox (10, Pos.CENTER) {
         label("Enter size of game field")
 
         textfield(size)
