2c7a4c83b854132e9feba05263c6ba7d50c70961
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-06-06 15:29:40 +0300

    Updated tests

--- src/test/kotlin/homeworks/homework4/task2/ParsingTreeTest.kt
+++ src/test/kotlin/homeworks/homework4/task2/ParsingTreeTest.kt
@@ -9,43 +9,39 @@ internal class ParsingTreeTest {
 
     @Test
     fun calculate_NumberThenNumber_MustWork() {
-        val parsingTree = ParsingTree()
-        parsingTree.parseFile(ByteArrayInputStream("(+ 3 4)".toByteArray()))
+        val parsingTree = ParsingTree(ByteArrayInputStream("(+ 3 4)".toByteArray()))
         assertEquals(7, parsingTree.calculate())
     }
 
     @Test
     fun calculate_NumberThenBrackets_MustWork() {
-        val parsingTree = ParsingTree()
-        parsingTree.parseFile(ByteArrayInputStream("(- 3 (* 4 10))".toByteArray()))
+        val parsingTree = ParsingTree(ByteArrayInputStream("(- 3 (* 4 10))".toByteArray()))
         assertEquals(-37, parsingTree.calculate())
     }
 
     @Test
     fun calculate_BracketsThenNumber_MustWork() {
-        val parsingTree = ParsingTree()
-        parsingTree.parseFile(ByteArrayInputStream("(/ (* 3 13) 3)".toByteArray()))
+        val parsingTree = ParsingTree(ByteArrayInputStream("(/ (* 3 13) 3)".toByteArray()))
         assertEquals(13, parsingTree.calculate())
     }
 
     @Test
     fun calculate_BracketsThenBrackets_MustWork() {
-        val parsingTree = ParsingTree()
-        parsingTree.parseFile(ByteArrayInputStream("(* (+ 3 13) (/ 144 12))".toByteArray()))
+        val parsingTree = ParsingTree(ByteArrayInputStream("(* (+ 3 13) (/ 144 12))".toByteArray()))
         assertEquals(192, parsingTree.calculate())
     }
 
     @Test
     fun calculate_ComplexTreeWithAllOperations_MustWork() {
-        val parsingTree = ParsingTree()
-        parsingTree.parseFile(ByteArrayInputStream("(* (+ (- 2 5) (/ 10 2)) (/ (+ 43 5) (* 2 6)))".toByteArray()))
+        val parsingTree =
+            ParsingTree(ByteArrayInputStream("(* (+ (- 2 5) (/ 10 2)) (/ (+ 43 5) (* 2 6)))".toByteArray()))
         assertEquals(8, parsingTree.calculate())
     }
 
     @Test
-    fun calculate_ComplexTreeWithLotsOfBrackets() {
-        val parsingTree = ParsingTree()
-        parsingTree.parseFile(ByteArrayInputStream("(* (+ 1 (+ 1 (+ (+ (+ 1 (+ (+ 1 1) 1)) 1) 1))) 10)".toByteArray()))
+    fun calculate_ComplexTreeWithLotsOfBrackets_MustWork() {
+        val parsingTree =
+            ParsingTree(ByteArrayInputStream("(* (+ 1 (+ 1 (+ (+ (+ 1 (+ (+ 1 1) 1)) 1) 1))) 10)".toByteArray()))
         assertEquals(80, parsingTree.calculate())
     }
 }
