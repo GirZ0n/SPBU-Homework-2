91330f4f0f4e870dd99ebf6fa4780cd56ebc88f0
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-06-06 15:29:21 +0300

    Fixed issues

--- src/main/kotlin/homeworks/homework4/task2/ParsingTree.kt
+++ src/main/kotlin/homeworks/homework4/task2/ParsingTree.kt
@@ -2,85 +2,130 @@ package homeworks.homework4.task2
 
 import java.io.IOException
 import java.io.InputStream
-import java.util.Scanner
 import kotlin.IllegalStateException
 
-class ParsingTree {
-    private var root: Node? = null
-
-    fun calculate() = root?.calculate() ?: throw IllegalStateException("Tree is empty")
-
-    fun parseFile(input: InputStream) {
-        val inputString = input.bufferedReader().readLine() ?: throw IOException("Expression not found")
-        root = getNode(inputString.substring(1, inputString.lastIndex))
+class ParsingTree(input: InputStream) {
+    private var root: Node
+    init {
+       root = parseFile(input)
     }
 
-    fun print() = println(root?.toString() ?: throw IllegalStateException("Tree is empty"))
+    fun calculate() = root.calculate()
+
+    private fun parseFile(input: InputStream): Node {
+        val inputString = input.bufferedReader().readLine() ?: throw IOException("Expression not found")
+        return getNode(inputString.substring(1, inputString.lastIndex))
+    }
+
+    fun print() = println(root.toString())
 
     private fun getNode(input: String): Node {
-        var scan = Scanner(input)
-        val result: Node = Operator(scan.next()[0])
-        if (scan.hasNextInt()) { // Case: "operator number ..."
-            result.leftChild = Argument(scan.nextInt())
-            if (scan.hasNextInt()) { // Case: "operator number number"
-                result.rightChild = Argument(scan.nextInt())
-            } else { // Case: "operator number (...)"
-                numberThenBracketsHandling(input, result)
-            }
-        } else { // Case: "operator (...) ..."
-            scan = Scanner(input.reversed())
-            if (scan.hasNextInt()) { // Case: "operator (...) number"
-                bracketsThenNumberHandling(input, result)
-            } else { // Case: "operator (...) (...)"
-                bracketsThenBracketsHandling(input, result)
-            }
+        if (isNumber(input)) {
+            return Argument(input.toInt())
         }
-        return result
+
+        val currentString = input.substring(1)
+        val areasOfInterest = mutableListOf<Pair<Int, Int>>()
+
+        getBracketsIndices(currentString, areasOfInterest)
+
+        if (areasOfInterest.size != 2) {
+            getNumbersIndices(currentString, areasOfInterest)
+        }
+
+        areasOfInterest.sortWith(compareBy { it.first })
+
+        val i1 = areasOfInterest[0].first
+        val j1 = areasOfInterest[0].second
+        val i2 = areasOfInterest[1].first
+        val j2 = areasOfInterest[1].second
+
+        val node = Operator(input[0])
+        node.leftChild = getNode(currentString.substring(i1..j1))
+        node.rightChild = getNode(currentString.substring(i2..j2))
+        return node
     }
 
-    private fun numberThenBracketsHandling(input: String, result: Node) { // Case: "operator number (...)"
-        val operandStartIndex = input.indexOf('(')
-        val operandEndIndex = input.lastIndexOf(')')
-        result.rightChild = getNode(input.substring(operandStartIndex + 1 until operandEndIndex))
-    }
-
-    private fun bracketsThenNumberHandling(input: String, result: Node) { // Case: "operator (...) number"
-        val operandStartIndex = input.indexOf('(')
-        val operandEndIndex = input.lastIndexOf(')')
-        result.leftChild = getNode(input.substring(operandStartIndex + 1 until operandEndIndex))
-        val scan = Scanner(input.substring(operandEndIndex + 1))
-        result.rightChild = Argument(scan.nextInt())
-    }
-
-    private fun bracketsThenBracketsHandling(input: String, result: Node) { // Case: "operator (...) (...)"
-        var operandStartIndex = input.indexOf('(')
-        var operandEndIndex = input.lastIndexOf(')')
+    // Здесь мы ищем первые и второе скобки (возможно не найдём)
+    private fun getBracketsIndices(input: String, areasOfInterest: MutableList<Pair<Int, Int>>) {
+        var bracketBeginIndex = input.indexOf("(")
         var balance = 0
-        for (i in operandStartIndex until input.lastIndex - operandStartIndex) {
+        var i = bracketBeginIndex
+        while (i in input.indices) {
             if (input[i] == '(') {
                 balance++
             } else if (input[i] == ')') {
                 balance--
             }
             if (balance == 0) {
-                operandEndIndex = i
-                result.leftChild = getNode(input.substring(operandStartIndex + 1 until operandEndIndex))
-                break
+                areasOfInterest.add(Pair(bracketBeginIndex + 1, i - 1))
+                bracketBeginIndex = input.substring(i + 1).indexOf("(") + i + 1
+                i = bracketBeginIndex
+                balance = 0
+                continue
+            }
+            i++
+        }
+    }
+
+    // Здесь мы ищем числа (возможно одно или два)
+    private fun getNumbersIndices(input: String, areasOfInterest: MutableList<Pair<Int, Int>>) {
+        val forbiddenArea = if (areasOfInterest.isNotEmpty()) { // индексы, находящиеся в скобках
+            val pair = areasOfInterest[0]
+            (pair.first)..(pair.second)
+        } else IntRange.EMPTY
+
+        var isNumber = false
+        var numberBeginIndex = input.indexOfFirst { it.isDigit() }
+        var i = numberBeginIndex
+        while (i in input.indices) {
+            if (i in forbiddenArea) {
+                i++
+                continue
+            }
+
+            when (input[i].isDigit()) {
+                true -> {
+                    if (!isNumber) {
+                        isNumber = true
+                        numberBeginIndex = i
+                    }
+                }
+                false -> {
+                    if (isNumber) {
+                        isNumber = false
+                        areasOfInterest.add(Pair(numberBeginIndex, i - 1))
+                    }
+                }
+            }
+            i++
+        }
+
+        if (isNumber) {
+            areasOfInterest.add(Pair(numberBeginIndex, i - 1))
+        }
+    }
+
+    private fun isNumber(input: String): Boolean {
+        for (char in input) {
+            if (!char.isDigit()) {
+                return false
             }
         }
-        operandStartIndex = input.substring(operandEndIndex + 1).indexOf('(') + operandEndIndex + 1
-        operandEndIndex += input.substring(operandEndIndex + 1).lastIndexOf(')') + 1
-        result.rightChild = getNode(input.substring(operandStartIndex + 1 until operandEndIndex))
+        return true
     }
 
-    private abstract class Node {
-        var leftChild: Node? = null
-        var rightChild: Node? = null
-        abstract fun calculate(): Int
-        abstract override fun toString(): String
+    private interface Node {
+        var leftChild: Node?
+        var rightChild: Node?
+        fun calculate(): Int
+        override fun toString(): String
     }
 
-    private class Operator(private val sign: Char) : Node() {
+    private class Operator(private val sign: Char) : Node {
+        override var leftChild: Node? = null
+        override var rightChild: Node? = null
+
         override fun toString(): String {
             val leftChildAsString = leftChild?.toString() ?: throw IllegalStateException("The left operand is missing")
             val rightChildAsString =
@@ -101,7 +146,10 @@ class ParsingTree {
         }
     }
 
-    private class Argument(private val value: Int) : Node() {
+    private class Argument(private val value: Int) : Node {
+        override var leftChild: Node? = null
+        override var rightChild: Node? = null
+
         override fun toString() = value.toString()
         override fun calculate() = value
     }
