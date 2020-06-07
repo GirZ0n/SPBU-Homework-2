b7352c24d0eb99a225b0a81143f7558ff710a073
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-06-07 20:44:43 +0300

    Fixed issues

--- src/main/kotlin/homeworks/homework4/task2/ParsingTree.kt
+++ src/main/kotlin/homeworks/homework4/task2/ParsingTree.kt
@@ -6,136 +6,91 @@ import kotlin.IllegalStateException
 
 class ParsingTree(input: InputStream) {
     private var root: Node
+
     init {
-       root = parseFile(input)
+        root = parseFile(input)
     }
 
     fun calculate() = root.calculate()
 
     private fun parseFile(input: InputStream): Node {
         val inputString = input.bufferedReader().readLine() ?: throw IOException("Expression not found")
-        return getNode(inputString.substring(1, inputString.lastIndex))
+        return getNode(inputString)
     }
 
     fun print() = println(root.toString())
 
     private fun getNode(input: String): Node {
-        if (isNumber(input)) {
+        val currentString = if (input[0] == '(') input.substring(1, input.lastIndex) else input
+
+        if (isNumber(currentString)) {
             return Argument(input.toInt())
         }
 
-        val currentString = input.substring(1)
-        val areasOfInterest = mutableListOf<Pair<Int, Int>>()
+        val firstTerm = getTermArea(currentString.substring(2))
+        firstTerm.begin += 2
+        firstTerm.end += 2
 
-        getBracketsIndices(currentString, areasOfInterest)
+        val secondTerm = getTermArea(currentString.substring(firstTerm.end + 2))
+        secondTerm.begin += firstTerm.end + 2
+        secondTerm.end += firstTerm.end + 2
 
-        if (areasOfInterest.size != 2) {
-            getNumbersIndices(currentString, areasOfInterest)
-        }
-
-        areasOfInterest.sortWith(compareBy { it.first })
-
-        val i1 = areasOfInterest[0].first
-        val j1 = areasOfInterest[0].second
-        val i2 = areasOfInterest[1].first
-        val j2 = areasOfInterest[1].second
-
-        val node = Operator(input[0])
-        node.leftChild = getNode(currentString.substring(i1..j1))
-        node.rightChild = getNode(currentString.substring(i2..j2))
-        return node
+        return Operator(
+            currentString[0],
+            getNode(currentString.substring(IntRange(firstTerm.begin, firstTerm.end))),
+            getNode(currentString.substring(IntRange(secondTerm.begin, secondTerm.end)))
+        )
     }
 
-    // Здесь мы ищем первые и второе скобки (возможно не найдём)
-    private fun getBracketsIndices(input: String, areasOfInterest: MutableList<Pair<Int, Int>>) {
-        var bracketBeginIndex = input.indexOf("(")
+    private fun getTermArea(input: String): Area {
+        return if (input[0] == '(') {
+            getBracketArea(input)
+        } else {
+            getNumberArea(input)
+        }
+    }
+
+    private fun getNumberArea(input: String): Area {
+        var indexOfEnd = 0
+        while (indexOfEnd in input.indices && input[indexOfEnd].isDigit()) {
+            indexOfEnd++
+        }
+        return Area(0, indexOfEnd - 1)
+    }
+
+    private fun getBracketArea(input: String): Area {
         var balance = 0
-        var i = bracketBeginIndex
-        while (i in input.indices) {
-            if (input[i] == '(') {
+        var indexOfEnd = 0
+        while (indexOfEnd in input.indices) {
+            if (input[indexOfEnd] == '(') {
                 balance++
-            } else if (input[i] == ')') {
+            } else if (input[indexOfEnd] == ')') {
                 balance--
             }
             if (balance == 0) {
-                areasOfInterest.add(Pair(bracketBeginIndex + 1, i - 1))
-                bracketBeginIndex = input.substring(i + 1).indexOf("(") + i + 1
-                i = bracketBeginIndex
-                balance = 0
-                continue
+                return Area(0, indexOfEnd)
             }
-            i++
+            indexOfEnd++
         }
+        return Area(-1, -1)
     }
 
-    // Здесь мы ищем числа (возможно одно или два)
-    private fun getNumbersIndices(input: String, areasOfInterest: MutableList<Pair<Int, Int>>) {
-        val forbiddenArea = if (areasOfInterest.isNotEmpty()) { // индексы, находящиеся в скобках
-            val pair = areasOfInterest[0]
-            (pair.first)..(pair.second)
-        } else IntRange.EMPTY
-
-        var isNumber = false
-        var numberBeginIndex = input.indexOfFirst { it.isDigit() }
-        var i = numberBeginIndex
-        while (i in input.indices) {
-            if (i in forbiddenArea) {
-                i++
-                continue
-            }
-
-            when (input[i].isDigit()) {
-                true -> {
-                    if (!isNumber) {
-                        isNumber = true
-                        numberBeginIndex = i
-                    }
-                }
-                false -> {
-                    if (isNumber) {
-                        isNumber = false
-                        areasOfInterest.add(Pair(numberBeginIndex, i - 1))
-                    }
-                }
-            }
-            i++
-        }
-
-        if (isNumber) {
-            areasOfInterest.add(Pair(numberBeginIndex, i - 1))
-        }
-    }
-
-    private fun isNumber(input: String): Boolean {
-        for (char in input) {
-            if (!char.isDigit()) {
-                return false
-            }
-        }
-        return true
-    }
+    private fun isNumber(input: String) = input.toIntOrNull() != null
 
     private interface Node {
-        var leftChild: Node?
-        var rightChild: Node?
         fun calculate(): Int
-        override fun toString(): String
     }
 
-    private class Operator(private val sign: Char) : Node {
-        override var leftChild: Node? = null
-        override var rightChild: Node? = null
-
+    private class Operator(private val sign: Char, var leftChild: Node, var rightChild: Node) : Node {
         override fun toString(): String {
-            val leftChildAsString = leftChild?.toString() ?: throw IllegalStateException("The left operand is missing")
-            val rightChildAsString =
-                rightChild?.toString() ?: throw IllegalStateException("The right operand is missing")
+            val leftChildAsString = leftChild.toString()
+            val rightChildAsString = rightChild.toString()
             return "($sign $leftChildAsString $rightChildAsString)"
         }
 
         override fun calculate(): Int {
-            val leftResult = leftChild?.calculate() ?: throw IllegalStateException("The left operand is missing")
-            val rightResult = rightChild?.calculate() ?: throw IllegalStateException("The right operand is missing")
+            val leftResult = leftChild.calculate()
+            val rightResult = rightChild.calculate()
             return when (sign) {
                 '+' -> leftResult + rightResult
                 '-' -> leftResult - rightResult
@@ -147,9 +102,6 @@ class ParsingTree(input: InputStream) {
     }
 
     private class Argument(private val value: Int) : Node {
-        override var leftChild: Node? = null
-        override var rightChild: Node? = null
-
         override fun toString() = value.toString()
         override fun calculate() = value
     }
