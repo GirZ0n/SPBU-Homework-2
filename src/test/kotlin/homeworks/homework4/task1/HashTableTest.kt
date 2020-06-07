169d2e1fb27429843916b72bedbee3f2d6a39414
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-06-07 19:08:17 +0300

    Added new tests

--- src/test/kotlin/homeworks/homework4/task1/HashTableTest.kt
+++ src/test/kotlin/homeworks/homework4/task1/HashTableTest.kt
@@ -4,21 +4,19 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
-const val INIT_SIZE = 1024
-
 internal class HashTableTest {
     private val simpleHashFunction = SimpleHashFunctionForString()
     private val polynomialHashFunction = PolynomialHashFunctionForString()
 
     @Test
     fun numberOfEntries_EmptyHashTable_MustWork() {
-        val hashTable = HashTable<String, Int>(INIT_SIZE, polynomialHashFunction)
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
         assertEquals(0, hashTable.numberOfEntries)
     }
 
     @Test
     fun numberOfEntries_NonEmptyHashTable_AddAndRemoveElements_MustWork() {
-        val hashTable = HashTable<String, Int>(INIT_SIZE, polynomialHashFunction)
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
         hashTable.add("qwerty", 6)
         hashTable.add("q", 6124)
         hashTable.add("4kegnu934h", 32523)
@@ -31,19 +29,19 @@ internal class HashTableTest {
 
     @Test
     fun find_emptyHashTable_MustWork() {
-        val hashTable = HashTable<String, Int>(INIT_SIZE, polynomialHashFunction)
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
         assertNull(hashTable.find("124f"))
     }
 
     @Test
     fun find_nullKey_MustWork() {
-        val hashTable = HashTable<String, Int>(INIT_SIZE, polynomialHashFunction)
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
         assertNull(hashTable.find(null))
     }
 
     @Test
     fun find_keyIsInHashTable_MustWork() {
-        val hashTable = HashTable<String, Int>(INIT_SIZE, polynomialHashFunction)
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
         hashTable.add("qwerty", 6)
         hashTable.add("q", 6124)
         hashTable.add("4kegnu934h", 32523)
@@ -54,7 +52,7 @@ internal class HashTableTest {
 
     @Test
     fun find_keyIsNotInHashTable_MustWork() {
-        val hashTable = HashTable<String, Int>(INIT_SIZE, polynomialHashFunction)
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
         hashTable.add("qwerty", 6)
         hashTable.add("q", 6124)
         hashTable.add("4kegnu934h", 32523)
@@ -64,8 +62,8 @@ internal class HashTableTest {
     }
 
     @Test
-    fun changeHashFunction_fromPolynomialToSimple_MustWork() {
-        val hashTable = HashTable<String, Int>(INIT_SIZE, polynomialHashFunction)
+    fun changeHashFunction_fromPolynomialToSimple_Find_MustWork() {
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
         hashTable.add("qwerty", 6)
         hashTable.add("q", 6124)
         hashTable.add("4kegnu934h", 32523)
@@ -76,8 +74,20 @@ internal class HashTableTest {
     }
 
     @Test
-    fun changeHashFunction_fromSimpleToPolynomial_MustWork() {
-        val hashTable = HashTable<String, Int>(INIT_SIZE, simpleHashFunction)
+    fun changeHashFunction_fromPolynomialToSimple_NumberOfEntries_MustWork() {
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
+        hashTable.add("qwerty", 6)
+        hashTable.add("q", 6124)
+        hashTable.add("4kegnu934h", 32523)
+        hashTable.add("5451916", 2)
+        hashTable.add("d,gnoi3nt34", 0)
+        hashTable.changeHashFunction(simpleHashFunction)
+        assertEquals(5, hashTable.numberOfEntries)
+    }
+
+    @Test
+    fun changeHashFunction_fromSimpleToPolynomial_Find_MustWork() {
+        val hashTable = HashTable<String, Int>(simpleHashFunction)
         hashTable.add("qwerty", 6)
         hashTable.add("q", 6124)
         hashTable.add("4kegnu934h", 32523)
@@ -85,5 +95,38 @@ internal class HashTableTest {
         hashTable.add("d,gnoi3nt34", 0)
         hashTable.changeHashFunction(polynomialHashFunction)
         assertEquals(6124, hashTable.find("q"))
+    }
+
+    @Test
+    fun changeHashFunction_fromSimpleToPolynomial_NumberOfEntries_MustWork() {
+        val hashTable = HashTable<String, Int>(simpleHashFunction)
+        hashTable.add("qwerty", 6)
+        hashTable.add("q", 6124)
+        hashTable.add("4kegnu934h", 32523)
+        hashTable.add("5451916", 2)
+        hashTable.add("d,gnoi3nt34", 0)
+        hashTable.changeHashFunction(polynomialHashFunction)
+        assertEquals(5, hashTable.numberOfEntries)
+    }
+
+    @Test
+    fun addAndRemove_OneElement_MustWork() {
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
+        hashTable.add("qwerty", 38258)
+        hashTable.remove("qwerty")
+        assertEquals(0, hashTable.numberOfEntries)
+    }
+
+    @Test
+    fun addAndRemove_ManyElements_MustWork() {
+        val hashTable = HashTable<String, Int>(polynomialHashFunction)
+        for (i in 0..1000) {
+            hashTable.add("$i", i)
+        }
+
+        for (i in 0..1000) {
+            hashTable.remove("$i")
+        }
+        assertEquals(0, hashTable.numberOfEntries)
     }
 }
