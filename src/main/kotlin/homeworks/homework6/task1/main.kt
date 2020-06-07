457e69a9da8982a743044ecdee89ec2e91ecbac4
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-06-07 15:54:22 +0300

    Moved all sorts in QuickSort class

--- src/main/kotlin/homeworks/homework6/task1/main.kt
+++ src/main/kotlin/homeworks/homework6/task1/main.kt
@@ -5,74 +5,23 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
-fun <T> quickSort(array: Array<T>, indexBegin: Int, indexEnd: Int) where T : Comparable<T> {
-    if (indexBegin < indexEnd) {
-        val middle = array[(indexBegin + indexEnd) / 2]
-        var i = indexBegin
-        var j = indexEnd
-        while (i <= j) {
-            while (array[i] < middle) {
-                i++
-            }
-            while (array[j] > middle) {
-                j--
-            }
-            if (i >= j) {
-                break
-            }
-            val temporary = array[i]
-            array[i] = array[j]
-            array[j] = temporary
-            i++
-            j--
-        }
-        quickSort(array, indexBegin, j)
-        quickSort(array, j + 1, indexEnd)
-    }
-}
-
-suspend fun <T> asyncQuickSort(array: Array<T>, indexBegin: Int, indexEnd: Int) where T : Comparable<T> {
-    if (indexBegin < indexEnd) {
-        val middle = array[(indexBegin + indexEnd) / 2]
-        var i = indexBegin
-        var j = indexEnd
-        while (i <= j) {
-            while (array[i] < middle) {
-                i++
-            }
-            while (array[j] > middle) {
-                j--
-            }
-            if (i >= j) {
-                break
-            }
-            val temporary = array[i]
-            array[i] = array[j]
-            array[j] = temporary
-            i++
-            j--
-        }
-        asyncQuickSort(array, indexBegin, j)
-        asyncQuickSort(array, j + 1, indexEnd)
-    }
-}
-
-private const val SIZE_OF_ARRAY = 1_000_000
-private const val MAX_NUMBER = 10_000
+fun generateArray(sizeOfArray: Int = 1000, maxNumber: Int = 10000) =
+    Array(sizeOfArray) { Random.nextInt(-maxNumber, maxNumber) }
 
 fun main() {
-    val arrayToSort = Array(SIZE_OF_ARRAY) { Random.nextInt(-MAX_NUMBER, MAX_NUMBER) }
+    val arrayToSort = generateArray()
+    val quickSort = QuickSort()
 
     val arrayForUsualQuickSort = arrayToSort.clone()
     val usualQuickSortTime = measureTimeMillis {
-        quickSort(arrayForUsualQuickSort, 0, arrayForUsualQuickSort.lastIndex)
+        quickSort.simpleQuickSort(arrayForUsualQuickSort, 0, arrayForUsualQuickSort.lastIndex)
     }
 
     val arrayForAsyncQuickSort = arrayToSort.clone()
     val asyncQuickSortTime = measureTimeMillis {
         runBlocking {
             launch {
-                asyncQuickSort(arrayForAsyncQuickSort, 0, arrayForAsyncQuickSort.lastIndex)
+                quickSort.asyncQuickSort(arrayForAsyncQuickSort, 0, arrayForAsyncQuickSort.lastIndex)
             }
         }
     }
