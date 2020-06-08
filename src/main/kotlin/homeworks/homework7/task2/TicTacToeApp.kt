84ef13a4b11f33f3d4f0d0a8db0e802e345b98aa
Ilya Vlasov <ilyavlasov2011@gmail.com>
2020-06-08 13:01:38 +0300

    Removed global const

--- src/main/kotlin/homeworks/homework7/task2/TicTacToeApp.kt
+++ src/main/kotlin/homeworks/homework7/task2/TicTacToeApp.kt
@@ -7,10 +7,12 @@ import javafx.stage.Stage
 import tornadofx.App
 import tornadofx.launch
 
-private const val STAGE_HEIGHT = 400.0
-private const val STAGE_WIDTH = 400.0
-
 class TicTacToeApp : App(Menu::class, MenuStyle::class, GameFieldStyle::class) {
+    companion object {
+        private const val STAGE_HEIGHT = 400.0
+        private const val STAGE_WIDTH = 400.0
+    }
+
     override fun start(stage: Stage) {
         stage.height = STAGE_HEIGHT
         stage.width = STAGE_WIDTH
